902dac619e2071705063e2b8192c249b21ae0369
xaviguasch <xaviguasch@gmail.com>
2022-03-19 13:34:18 +0100

    added solution for 004-reversed-strings kata

--- 01-fundamentals/004-reversed-strings.js
+++ 01-fundamentals/004-reversed-strings.js
@@ -3,4 +3,16 @@
 // 'world'  =>  'dlrow'
 // 'word'   =>  'drow'
 
-function solution(str) {}
+function solution(str) {
+  const arr = str.split('')
+
+  let reversedStr = ''
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversedStr += arr[i]
+  }
+
+  return reversedStr
+}
+
+console.log(solution('world'))
