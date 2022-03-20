80dcf07d91dd6dd083f236799a24768c77607f0d
xaviguasch <xaviguasch@gmail.com>
2022-03-20 14:10:42 +0100

    added solution for previous kata

--- 01-fundamentals--8-kyu/009-find-the-smallest-integer-in-the-array.js
+++ 01-fundamentals--8-kyu/009-find-the-smallest-integer-in-the-array.js
@@ -6,6 +6,16 @@
 // Given [34, -345, -1, 100] your solution will return -345
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
-class SmallestIntegerFinder {
-  findSmallestInt(args) {}
+const findSmallestInt = (args) => {
+  let smallest = args[0]
+  args.forEach((el) => {
+    if (el < smallest) {
+      smallest = el
+    }
+  })
+
+  return smallest
 }
+
+console.log(findSmallestInt([34, 15, 88, 2]))
+console.log(findSmallestInt([34, -345, -1, 100]))
