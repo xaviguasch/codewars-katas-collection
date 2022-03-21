90dd6ca23c259685d987c37684d99e3dfd674091
xaviguasch <xaviguasch@gmail.com>
2022-03-21 11:09:23 +0100

    added alternative solutions to previous kata

--- 01-fundamentals--8-kyu/017-convert-a-string-to-a-number.js
+++ 01-fundamentals--8-kyu/017-convert-a-string-to-a-number.js
@@ -14,3 +14,13 @@ const stringToNumber = (str) => {
 }
 
 console.log(stringToNumber('1234'))
+
+// ALTERNATIVE SOLUTIONS
+
+// var stringToNumber = function(str){
+//   return parseInt(str);
+// }
+
+// var stringToNumber = function(str){
+//   return +str;
+// }
