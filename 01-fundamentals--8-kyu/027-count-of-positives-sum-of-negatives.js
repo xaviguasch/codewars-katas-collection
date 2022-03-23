5aca2ccf77dcc113135b7bbf4a883df7122eddaa
xaviguasch <xaviguasch@gmail.com>
2022-03-23 12:26:03 +0100

    added alternative solutions to previous kata

--- 01-fundamentals--8-kyu/027-count-of-positives-sum-of-negatives.js
+++ 01-fundamentals--8-kyu/027-count-of-positives-sum-of-negatives.js
@@ -23,3 +23,28 @@ console.log(
 )
 
 console.log(countPositivesSumNegatives([0, 0]))
+console.log(countPositivesSumNegatives([]))
+
+// ALTERNATIVE SOLUTIONS
+
+//  function countPositivesSumNegatives(input) {
+//     if (input == null || input.length == 0)
+//       return [];
+
+//     var positive = 0;
+//     var negative = 0;
+
+//     for (var i=0, l=input.length; i<l; ++i)
+//     {
+//       if (input[i] > 0)
+//         ++ positive;
+//       else
+//         negative += input[i];
+//     }
+
+//     return [positive, negative];
+// }
+
+// function countPositivesSumNegatives(input) {
+//     return input && input.length ? [input.filter(p => p > 0).length, input.filter(n => n < 0).reduce((a, b) => a + b, 0)] : [];
+// }
