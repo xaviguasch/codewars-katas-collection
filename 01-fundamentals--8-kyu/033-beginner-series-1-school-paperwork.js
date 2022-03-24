182311bc73558f06152f5f3620ec43abc30b43b9
xaviguasch <xaviguasch@gmail.com>
2022-03-24 12:17:18 +0100

    adjusted previous solution for edge case

--- 01-fundamentals--8-kyu/033-beginner-series-1-school-paperwork.js
+++ 01-fundamentals--8-kyu/033-beginner-series-1-school-paperwork.js
@@ -6,7 +6,7 @@
 // n= 5, m=5: 25
 // n=-5, m=5:  0
 
-const paperwork = (n, m) => (n > 0 && m > 0 ? n * m : 0)
+const paperwork = (n, m) => (n > 0 || m > 0 ? n * m : 0)
 
 console.log(paperwork(5, 5))
 console.log(paperwork(5, -5))
