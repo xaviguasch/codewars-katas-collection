cf04e6cca3b91144ea1ac79d12991ab909f6bcc1
xaviguasch <xaviguasch@gmail.com>
2022-03-26 22:59:52 +0100

    added 038-are-you-playing-banjo kata

---
+++ 01-fundamentals--8-kyu/038-are-you-playing-banjo.js
@@ -0,0 +1,16 @@
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo"
+// name + " does not play banjo"
+// Names given are always valid strings.
+
+const areYouPlayingBanjo = (name) => {
+  return name
+}
+
+console.log(areYouPlayingBanjo('Roger'))
+console.log(areYouPlayingBanjo('Mary'))
+console.log(areYouPlayingBanjo('ryan'))
