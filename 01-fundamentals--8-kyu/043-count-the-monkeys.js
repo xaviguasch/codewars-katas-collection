37445b826c3305415cf281022a8d3ce68d668da8
xaviguasch <xaviguasch@gmail.com>
2022-03-27 21:07:35 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/043-count-the-monkeys.js
+++ 01-fundamentals--8-kyu/043-count-the-monkeys.js
@@ -7,7 +7,15 @@
 // monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 // monkeyCount(1) // --> [1]
 
-const monkeyCount = (n) => {}
+const monkeyCount = (n) => {
+  const newArr = []
+
+  for (let i = 1; i <= n; i++) {
+    newArr.push(i)
+  }
+
+  return newArr
+}
 
 console.log(monkeyCount(10))
 console.log(monkeyCount(1))
