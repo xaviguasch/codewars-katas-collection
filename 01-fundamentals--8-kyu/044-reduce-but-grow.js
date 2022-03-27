7f6d9fedad5f45218ec50764d89c875e687f8be8
xaviguasch <xaviguasch@gmail.com>
2022-03-27 21:11:45 +0200

    added 044-beginner-reduce-grow kata

---
+++ 01-fundamentals--8-kyu/044-reduce-but-grow.js
@@ -0,0 +1,10 @@
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+const grow = (x) => {}
+
+console.log(grow([1, 2, 3, 4]))
+console.log(grow([1, 2, 3]))
+console.log(grow([4, 1, 1, 1, 4]))
+console.log(grow([2, 2, 2, 2, 2, 2]))
