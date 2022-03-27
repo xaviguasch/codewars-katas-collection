5cb5b082938dc409706255ed6261a870e1662f5b
xaviguasch <xaviguasch@gmail.com>
2022-03-27 20:59:30 +0200

    added 042-simple-multiplication kata

---
+++ 01-fundamentals--8-kyu/042-simple-multiplication.js
@@ -0,0 +1,8 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+const simpleMultiplication = (number) => {}
+
+console.log(simpleMultiplication(2))
+console.log(simpleMultiplication(1))
+console.log(simpleMultiplication(8))
+console.log(simpleMultiplication(4))
