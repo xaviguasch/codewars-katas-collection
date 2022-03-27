b6cec40c76006a005e448d3bae63f1d6b5271884
xaviguasch <xaviguasch@gmail.com>
2022-03-27 21:01:45 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/042-simple-multiplication.js
+++ 01-fundamentals--8-kyu/042-simple-multiplication.js
@@ -1,6 +1,6 @@
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
-const simpleMultiplication = (number) => {}
+const simpleMultiplication = (number) => (number % 2 === 0 ? number * 8 : number * 9)
 
 console.log(simpleMultiplication(2))
 console.log(simpleMultiplication(1))
