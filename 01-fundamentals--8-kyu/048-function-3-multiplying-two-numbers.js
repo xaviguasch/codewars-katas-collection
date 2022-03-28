eac640c2c838982cb8773abecccff690d7154d31
xaviguasch <xaviguasch@gmail.com>
2022-03-28 11:55:20 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/048-function-3-multiplying-two-numbers.js
+++ 01-fundamentals--8-kyu/048-function-3-multiplying-two-numbers.js
@@ -1,5 +1,6 @@
 // Implement a function which multiplies two numbers.
 
-const multiplyTwoNumbers = (n1, n2) => {}
+const multiplyTwoNumbers = (n1, n2) => n1 * n2
 
 console.log(multiplyTwoNumbers(4, 5))
+console.log(multiplyTwoNumbers(2, 3))
