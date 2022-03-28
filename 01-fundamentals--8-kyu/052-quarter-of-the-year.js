f810ebfd9fb10dfd1f4037aab172fa07b0ce9b08
xaviguasch <xaviguasch@gmail.com>
2022-03-28 19:00:08 +0200

    added previous solutions

--- 01-fundamentals--8-kyu/052-quarter-of-the-year.js
+++ 01-fundamentals--8-kyu/052-quarter-of-the-year.js
@@ -12,3 +12,18 @@ const quarterOf = (month) => {
 console.log(quarterOf(3))
 console.log(quarterOf(8))
 console.log(quarterOf(11))
+
+// ALTERNATIVE SOLUTIONS
+
+// const quarterOf = m => Math.ceil(m/3);
+
+// const quarterOf = (month) => {
+//   if (month <= 3) {
+//     return 1
+//   } else if (month <= 6) {
+//     return 2
+//   } else if (month <= 9) {
+//     return 3
+//   } else if (month <= 12) {
+//     return 4
+//   }
