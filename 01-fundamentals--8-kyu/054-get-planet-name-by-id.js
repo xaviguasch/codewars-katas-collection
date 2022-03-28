e390bfaf1b03594599103fbe365d52e2a7ac90de
xaviguasch <xaviguasch@gmail.com>
2022-03-28 19:38:37 +0200

    added 054-get-planet-name-by-id kata

---
+++ 01-fundamentals--8-kyu/054-get-planet-name-by-id.js
@@ -0,0 +1,33 @@
+// The function is not returning the correct values. Can you figure out why?
+
+// Example (Input --> Output ):
+
+// 3 --> "Earth"
+
+const getPlanetName = (id) => {
+  var name
+  switch (id) {
+    case 1:
+      name = 'Mercury'
+    case 2:
+      name = 'Venus'
+    case 3:
+      name = 'Earth'
+    case 4:
+      name = 'Mars'
+    case 5:
+      name = 'Jupiter'
+    case 6:
+      name = 'Saturn'
+    case 7:
+      name = 'Uranus'
+    case 8:
+      name = 'Neptune'
+  }
+
+  return name
+}
+
+console.log(getPlanetName(2))
+console.log(getPlanetName(5))
+console.log(getPlanetName(3))
