ba832b593da4cf34d6351e0066d4811fddfc5791
xaviguasch <xaviguasch@gmail.com>
2022-03-29 17:52:27 +0200

    added 057-remove-exclamation-marks kata

---
+++ 01-fundamentals--8-kyu/057-remove-exclamation-marks.js
@@ -0,0 +1,7 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+const removeExclamationMarks = (s) => {
+  return ''
+}
+
+console.log(removeExclamationMarks('Hello World!'))
