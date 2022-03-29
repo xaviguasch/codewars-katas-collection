875209799e71713ac673952d77dfb2554a446c9e
xaviguasch <xaviguasch@gmail.com>
2022-03-29 17:47:25 +0200

    added 056-third-angle-of-a-triangle kata

---
+++ 01-fundamentals--8-kyu/056-third-angle-of-a-triangle.js
@@ -0,0 +1,15 @@
+// You are given two interior angles (in degrees) of a triangle.
+
+// Write a function to return the 3rd.
+
+// Note: only positive integers will be tested.
+
+// https://en.wikipedia.org/wiki/Triangle
+
+const otherAngle = (a, b) => {
+  return
+}
+
+console.log(otherAngle(30, 60))
+console.log(otherAngle(60, 60))
+console.log(otherAngle(43, 150))
