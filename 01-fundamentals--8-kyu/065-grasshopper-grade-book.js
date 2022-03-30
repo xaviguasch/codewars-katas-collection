494bfb824b956aab78334b35dda65c811b8f31dd
xaviguasch <xaviguasch@gmail.com>
2022-03-30 23:37:03 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/065-grasshopper-grade-book.js
+++ 01-fundamentals--8-kyu/065-grasshopper-grade-book.js
@@ -10,7 +10,13 @@
 // Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
 
 const getGrade = (s1, s2, s3) => {
-  return
+  const average = (s1 + s2 + s3) / 3
+
+  if (average >= 90) return 'A'
+  else if (average >= 80) return 'B'
+  else if (average >= 70) return 'C'
+  else if (average >= 60) return 'D'
+  else return 'F'
 }
 
 console.log(getGrade(95, 90, 93))
