e7eee8cc65f405e277d6bf464bfaed5a77b5bb62
xaviguasch <xaviguasch@gmail.com>
2022-03-30 23:05:45 +0200

    added 061-if-you-cant-sleep-count-sheep kata

---
+++ 01-fundamentals--8-kyu/061-if-you-cant-sleep-count-sheep.js
@@ -0,0 +1,13 @@
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+const countSheep = (num) => {
+  return
+}
+
+console.log(countSheep(0))
+console.log(countSheep(1))
+console.log(countSheep(2))
+console.log(countSheep(3))
