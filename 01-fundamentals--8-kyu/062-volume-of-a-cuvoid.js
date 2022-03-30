206bb981aeab4670cd1df3f6b252168621bed492
xaviguasch <xaviguasch@gmail.com>
2022-03-30 23:11:59 +0200

    added 062-volume-of-a-cuboid kata

---
+++ 01-fundamentals--8-kyu/062-volume-of-a-cuvoid.js
@@ -0,0 +1,8 @@
+// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid. Write a function to help Bob with this calculation.
+
+const getVolumeOfCuboid = (length, width, height) => {
+  return
+}
+
+console.log(getVolumeOfCuboid(1, 2, 2))
+console.log(getVolumeOfCuboid(6, 3, 2))
