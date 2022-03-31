640c42143ba453f889b4f500a5ea36c2ed55be1e
xaviguasch <xaviguasch@gmail.com>
2022-03-31 23:40:54 +0200

    added alternative solutions to previous kata

--- 01-fundamentals--8-kyu/070-will-there-be-enough-space.js
+++ 01-fundamentals--8-kyu/070-will-there-be-enough-space.js
@@ -17,3 +17,13 @@ const enough = (cap, on, wait) => (on + wait <= cap ? 0 : Math.abs(cap - on - wa
 
 console.log(enough(10, 5, 5))
 console.log(enough(100, 60, 50))
+
+// ALTERNATIVE SOLUTIONS
+
+// function enough(cap, on, wait) {
+//   return (on+wait > cap) ? on+wait-cap : 0;
+// }
+
+// function enough(cap, on, wait) {
+//   return Math.max(wait + on - cap, 0);
+// }
