aed875c1bb53a7ff2111439aa3f27c68af4a8952
xaviguasch <xaviguasch@gmail.com>
2022-03-31 23:11:22 +0200

    added 068-twice-as-old kata

---
+++ 01-fundamentals--8-kyu/068-twice-as-old.js
@@ -0,0 +1,12 @@
+// Your function takes two arguments:
+
+// current father's age (years)
+// current age of his son (years)
+// Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old).
+
+const twiceAsOld = (dadYearsOld, sonYearsOld) => {
+  return
+}
+
+console.log(twiceAsOld(36, 7))
+console.log(twiceAsOld(55, 30))
