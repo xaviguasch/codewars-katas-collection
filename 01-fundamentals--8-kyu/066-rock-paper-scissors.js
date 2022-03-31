400cb60970584b77c9c4da33a9099e79dfabf688
xaviguasch <xaviguasch@gmail.com>
2022-03-31 23:04:23 +0200

    added alternative solution to previous kata

--- 01-fundamentals--8-kyu/066-rock-paper-scissors.js
+++ 01-fundamentals--8-kyu/066-rock-paper-scissors.js
@@ -22,3 +22,15 @@ console.log(rps('rock', 'scissors'))
 console.log(rps('rock', 'paper'))
 console.log(rps('scissors', 'scissors'))
 console.log(rps('paper', 'rock'))
+
+// ALTERNATIVE SOLUTION
+
+const rps = (p1, p2) => {
+  if (p1 === p2) return 'Draw!'
+  var rules = { rock: 'scissors', paper: 'rock', scissors: 'paper' }
+  if (p2 === rules[p1]) {
+    return 'Player 1 won!'
+  } else {
+    return 'Player 2 won!'
+  }
+}
