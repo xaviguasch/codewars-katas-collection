580fc0919ed5cba1995e68503b989b857a6f340b
xaviguasch <xaviguasch@gmail.com>
2022-04-01 23:00:53 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/071-the-feast-of-many-beasts.js
+++ 01-fundamentals--8-kyu/071-the-feast-of-many-beasts.js
@@ -4,9 +4,9 @@
 
 // Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
 
-const feast = (beast, dish) => {
-  return
-}
+const feast = (beast, dish) =>
+  beast.split('')[0] === dish.split('')[0] &&
+  beast.split('')[beast.length - 1] === dish.split('')[dish.length - 1]
 
 console.log(feast('great blue heron', 'garlic naan'))
 console.log(feast('chickadee', 'chocolate cake'))
