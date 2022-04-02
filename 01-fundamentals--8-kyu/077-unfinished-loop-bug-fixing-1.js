1cac06e2c1752473f7f3a02d38483dfd0f65ab59
xaviguasch <xaviguasch@gmail.com>
2022-04-02 23:00:18 +0200

    added unfinished-loop-bug-fixing-1 kata

---
+++ 01-fundamentals--8-kyu/077-unfinished-loop-bug-fixing-1.js
@@ -0,0 +1,17 @@
+// Unfinished Loop - Bug Fixing #1
+// Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
+
+function createArray(number) {
+  var newArray = []
+
+  for (var counter = 1; counter <= number; ) {
+    newArray.push(counter)
+  }
+
+  return newArray
+}
+
+console.log(createArray(1))
+console.log(createArray(2))
+console.log(createArray(3))
+console.log(createArray(4))
