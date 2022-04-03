1762fec875c8478f5f68c58be01867cd8e2f4ee7
xaviguasch <xaviguasch@gmail.com>
2022-04-03 20:49:31 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/081-students-final-grade.js
+++ 01-fundamentals--8-kyu/081-students-final-grade.js
@@ -23,8 +23,17 @@
 // *Use Comparison and Logical Operators.
 
 const finalGrade = (exam, projects) => {
-  return
+  if (exam > 90 || projects > 10) {
+    return 100
+  } else if (exam > 75 && projects >= 5) {
+    return 90
+  } else if (exam > 50 && projects >= 2) {
+    return 75
+  } else {
+    return 0
+  }
 }
 
 console.log(finalGrade(100, 12))
 console.log(finalGrade(85, 5))
+console.log(finalGrade(55, 0))
