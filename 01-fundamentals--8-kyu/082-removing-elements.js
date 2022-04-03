ea64ef7587d56abaa5b608b90260859d7992bf9c
xaviguasch <xaviguasch@gmail.com>
2022-04-03 21:01:42 +0200

    added alternative solutions

--- 01-fundamentals--8-kyu/082-removing-elements.js
+++ 01-fundamentals--8-kyu/082-removing-elements.js
@@ -10,3 +10,21 @@ const removeEveryOther = (arr) => arr.filter((el, i) => i % 2 === 0)
 console.log(removeEveryOther(['Hello', 'Goodbye', 'Hello Again']))
 console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
 console.log(removeEveryOther([1, 2]))
+
+// ALTERNATIVE SOLUTIONS
+
+// function removeEveryOther(arr) {
+//   var newArr = []
+//   for (var i = 0; i < arr.length; i += 2) {
+//     newArr.push(arr[i])
+//   }
+//   return newArr
+// }
+
+// function removeEveryOther(arr){
+//   //your code here
+//   for (var i = 1; i < arr.length;i++){
+//       arr.splice(i,1);
+//   }
+//   return arr;
+// }
