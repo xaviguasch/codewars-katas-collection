b1386ca1837de23a777975a404522a3c6156317d
xaviguasch <xaviguasch@gmail.com>
2022-04-04 19:26:10 +0200

    added alternative solution to previous kata

--- 01-fundamentals--8-kyu/088-grasshopper-check-for-factor.js
+++ 01-fundamentals--8-kyu/088-grasshopper-check-for-factor.js
@@ -19,3 +19,7 @@ console.log(checkForFactor(10, 2))
 console.log(checkForFactor(63, 7))
 console.log(checkForFactor(9, 2))
 console.log(checkForFactor(653, 7))
+
+// ALTERNATIVE SOLUTION
+
+// const checkForFactor = (base, factor) => !(base % factor);
