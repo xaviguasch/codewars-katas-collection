f1d47ca0a0dccebab9df8aac213de81502bde0bd
xaviguasch <xaviguasch@gmail.com>
2022-04-04 18:58:48 +0200

    added 087-sort-and-star kata

---
+++ 01-fundamentals--8-kyu/087-sort-and-star.js
@@ -0,0 +1,30 @@
+// You will be given a vector of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+const twoSort = (s) => {
+  return
+}
+
+console.log(
+  twoSort(['bitcoin', 'take', 'over', 'the', 'world', 'maybe', 'who', 'knows', 'perhaps'])
+)
+
+console.log(
+  twoSort([
+    'turns',
+    'out',
+    'random',
+    'test',
+    'cases',
+    'are',
+    'easier',
+    'than',
+    'writing',
+    'out',
+    'basic',
+    'ones',
+  ])
+)
