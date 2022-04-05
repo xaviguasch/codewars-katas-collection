ba69c653fe3f6ff5cb7760fd7c441dbc29267e5c
xaviguasch <xaviguasch@gmail.com>
2022-04-05 11:10:08 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/093-well-of-ideas.js
+++ 01-fundamentals--8-kyu/093-well-of-ideas.js
@@ -3,7 +3,13 @@
 // In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
 
 const well = (x) => {
-  return
+  let goodIdeas = []
+
+  x.forEach((idea) => (idea === 'good' ? goodIdeas.push(idea) : ''))
+
+  if (goodIdeas.length > 2) return 'I smell a series!'
+  else if (goodIdeas.length > 0) return 'Publish!'
+  else return 'Fail!'
 }
 
 console.log(well(['bad', 'bad', 'bad']))
