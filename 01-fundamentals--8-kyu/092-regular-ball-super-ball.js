06eb3a2279dfde33c21341e89bdc8c4d1d07e751
xaviguasch <xaviguasch@gmail.com>
2022-04-05 10:53:14 +0200

    added 092-regular-ball-super-ball kata

---
+++ 01-fundamentals--8-kyu/092-regular-ball-super-ball.js
@@ -0,0 +1,16 @@
+// Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+// If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+// ball1 = new Ball();
+// ball2 = new Ball("super");
+
+// ball1.ballType     //=> "regular"
+// ball2.ballType     //=> "super"
+
+class Ball {
+  return
+}
+
+console.log(new Ball().ballType)
+console.log(new Ball('super').ballType)
