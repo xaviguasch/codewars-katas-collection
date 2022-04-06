6c8d134d1a082f74832146d7779ddcc8fdad1ea8
xaviguasch <xaviguasch@gmail.com>
2022-04-06 22:07:49 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/096-n-th-power.js
+++ 01-fundamentals--8-kyu/096-n-th-power.js
@@ -8,7 +8,11 @@
 // array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
 
 const index = (array, n) => {
-  return
+  if (n < array.length) {
+    return Math.pow(array[n], n)
+  } else {
+    return -1
+  }
 }
 
 console.log(index([1, 2, 3, 4], 2))
