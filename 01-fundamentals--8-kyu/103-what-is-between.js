5a81eb7456b828a234de1ae9c10deaa6acef4b2b
xaviguasch <xaviguasch@gmail.com>
2022-04-07 22:59:59 +0200

    added 103-what-is-between kata

---
+++ 01-fundamentals--8-kyu/103-what-is-between.js
@@ -0,0 +1,14 @@
+// Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+// For example:
+
+// a = 1
+// b = 4
+// --> [1, 2, 3, 4]
+
+const between = (a, b) => {
+  return
+}
+
+console.log(between(1, 4))
+console.log(between(-2, 2))
