09d4bc7fc64856322c888bb3b4e1a732a60aab15
xaviguasch <xaviguasch@gmail.com>
2022-04-07 23:27:10 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/105-cat-years-dog-years.js
+++ 01-fundamentals--8-kyu/105-cat-years-dog-years.js
@@ -18,7 +18,21 @@
 // +5 dog years for each year after that
 
 const humanYearsCatYearsDogYears = (humanYears) => {
-  return [0, 0, 0]
+  let catYears = 0
+  let dogYears = 0
+
+  if (humanYears === 1) {
+    catYears = 15
+    dogYears = 15
+  } else if (humanYears === 2) {
+    catYears = 24
+    dogYears = 24
+  } else if (humanYears > 2) {
+    catYears = 24 + (humanYears - 2) * 4
+    dogYears = 24 + (humanYears - 2) * 5
+  }
+
+  return [humanYears, catYears, dogYears]
 }
 
 console.log(humanYearsCatYearsDogYears(1))
