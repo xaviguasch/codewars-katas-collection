714f9aca95acd361f8583647aca6eb59dcac75f1
xaviguasch <xaviguasch@gmail.com>
2022-04-07 22:52:17 +0200

    added 102-is-it-palindrome kata

---
+++ 01-fundamentals--8-kyu/102-is-it-a-palindrome.js
@@ -0,0 +1,11 @@
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+
+const isPalindrome = (x) => {
+  return
+}
+
+console.log(isPalindrome('a'))
+console.log(isPalindrome('aba'))
+console.log(isPalindrome('Abba'))
+console.log(isPalindrome('hello'))
+console.log(isPalindrome('bob'))
