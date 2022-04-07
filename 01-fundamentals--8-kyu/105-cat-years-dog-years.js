07c355dce028bd4cbec5fe132a9441e97a7fd482
xaviguasch <xaviguasch@gmail.com>
2022-04-07 23:30:20 +0200

    added alternative solutions to previous kata

--- 01-fundamentals--8-kyu/105-cat-years-dog-years.js
+++ 01-fundamentals--8-kyu/105-cat-years-dog-years.js
@@ -38,3 +38,17 @@ const humanYearsCatYearsDogYears = (humanYears) => {
 console.log(humanYearsCatYearsDogYears(1))
 console.log(humanYearsCatYearsDogYears(2))
 console.log(humanYearsCatYearsDogYears(10))
+
+// ALTERNATIVE SOLUTIONS
+
+// var humanYearsCatYearsDogYears = function(y) {
+//   if (y == 1) return [1, 15, 15]
+//   if (y == 2) return [2, 24, 24]
+//   return [y, (y-2) * 4 + 24, (y-2) * 5 + 24]
+// }
+
+// const humanYearsCatYearsDogYears = humanYears => [
+//   humanYears,
+//   ( humanYears - 1 ? 16 : 11 ) + 4 * humanYears,
+//   ( humanYears - 1 ? 14 : 10 ) + 5 * humanYears,
+// ];
