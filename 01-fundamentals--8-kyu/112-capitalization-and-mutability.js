1e61bdb77befeb24bfe7061687d252f39aa99e1e
xaviguasch <xaviguasch@gmail.com>
2022-04-09 23:46:09 +0200

    added alternative solution

--- 01-fundamentals--8-kyu/112-capitalization-and-mutability.js
+++ 01-fundamentals--8-kyu/112-capitalization-and-mutability.js
@@ -9,3 +9,7 @@ const capitalizeWord = (word) => word[0].toUpperCase() + word.slice(1)
 console.log(capitalizeWord('word'))
 console.log(capitalizeWord('i'))
 console.log(capitalizeWord('glasswear'))
+
+// ALTERNATIVE SOLUTION
+
+// const capitalizeWord = (word) => word.replace(word.charAt(0), word.charAt(0).toUpperCase());
