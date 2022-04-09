7d814bc714d5bf419e9bab87849351bc37d53986
xaviguasch <xaviguasch@gmail.com>
2022-04-09 23:42:13 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/112-capitalization-and-mutability.js
+++ 01-fundamentals--8-kyu/112-capitalization-and-mutability.js
@@ -4,10 +4,7 @@
 
 // Don't worry about numbers, special characters, or non-string types being passed to the function. The string lengths will be from 1 character up to 10 characters, but will never be empty.
 
-const capitalizeWord = (word) => {
-  word[0].toUpperCase()
-  return word
-}
+const capitalizeWord = (word) => word[0].toUpperCase() + word.slice(1)
 
 console.log(capitalizeWord('word'))
 console.log(capitalizeWord('i'))
