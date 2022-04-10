101c42d5e419a66047026de4d49c6fede4e04750
xaviguasch <xaviguasch@gmail.com>
2022-04-10 23:14:53 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/117-remove-duplicates-from-list.js
+++ 01-fundamentals--8-kyu/117-remove-duplicates-from-list.js
@@ -3,7 +3,15 @@
 // The order of the sequence has to stay the same.
 
 const distinct = (a) => {
-  return []
+  let newArr = []
+
+  for (let i = 0; i <= a.length - 1; i++) {
+    if (newArr.indexOf(a[i]) < 0) {
+      newArr.push(a[i])
+    }
+  }
+
+  return newArr
 }
 
 console.log(distinct([1]))
