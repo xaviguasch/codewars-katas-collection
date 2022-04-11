c6be59846ee64e5de88c0455c02d9487c4ebe6d5
xaviguasch <xaviguasch@gmail.com>
2022-04-11 15:29:29 +0200

    added 119-grasshopper-function-syntax-debugging kata

---
+++ 01-fundamentals--8-kyu/119-grasshopper-function-syntax-debugging.js
@@ -0,0 +1,8 @@
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+const main = (verb, noum) => {
+  return verb + noum
+}
+
+console.log(main('take', 'item'))
+console.log(main('use', 'sword'))
