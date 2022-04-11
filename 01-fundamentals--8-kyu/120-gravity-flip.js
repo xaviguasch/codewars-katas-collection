8126cc6fb344ef5d1d33cdb06bf9190a8ae2bc8e
xaviguasch <xaviguasch@gmail.com>
2022-04-11 15:38:05 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/120-gravity-flip.js
+++ 01-fundamentals--8-kyu/120-gravity-flip.js
@@ -19,9 +19,7 @@
 // * 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
 // * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
 
-const flip = (d, a) => {
-  return
-}
+const flip = (d, a) => (d === 'R' ? a.sort((a, b) => a - b) : a.sort((a, b) => b - a))
 
 console.log(flip('R', [3, 2, 1, 2]))
 console.log(flip('L', [1, 4, 5, 3, 5]))
