a826324add2a5a03edddea8821cf477417ea5969
xaviguasch <xaviguasch@gmail.com>
2022-04-11 15:38:38 +0200

    added alternative solution to previous kata

--- 01-fundamentals--8-kyu/120-gravity-flip.js
+++ 01-fundamentals--8-kyu/120-gravity-flip.js
@@ -23,3 +23,7 @@ const flip = (d, a) => (d === 'R' ? a.sort((a, b) => a - b) : a.sort((a, b) => b
 
 console.log(flip('R', [3, 2, 1, 2]))
 console.log(flip('L', [1, 4, 5, 3, 5]))
+
+// ALTERNATIVE SOLUTION
+
+// const flip = (d, a) => a.sort((x, y) => d === 'R' ? x - y : y - x);
