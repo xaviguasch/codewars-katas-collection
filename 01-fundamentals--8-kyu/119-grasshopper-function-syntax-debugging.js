8ab5e9b55f7eeece29d552a249fab40998bc86f7
xaviguasch <xaviguasch@gmail.com>
2022-04-11 15:31:07 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/119-grasshopper-function-syntax-debugging.js
+++ 01-fundamentals--8-kyu/119-grasshopper-function-syntax-debugging.js
@@ -1,8 +1,6 @@
 // A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
 
-const main = (verb, noum) => {
-  return verb + noum
-}
+const main = (verb, noum) => verb + noum
 
-console.log(main('take', 'item'))
-console.log(main('use', 'sword'))
+console.log(main('take ', 'item'))
+console.log(main('use ', 'sword'))
