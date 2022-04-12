a362a9dc3d0594923bcd584735201afa6936b0e4
xaviguasch <xaviguasch@gmail.com>
2022-04-12 21:39:21 +0200

    added alternative solutions

--- 01-fundamentals--8-kyu/125-swap-values.js
+++ 01-fundamentals--8-kyu/125-swap-values.js
@@ -16,3 +16,22 @@ function swapValues() {
 console.log(swapValues([0, 1]))
 console.log(swapValues([1, 1]))
 console.log(swapValues([2, 9]))
+
+// ALTERNATIVE SOLUTIONS
+
+// function swapValues() {
+//   return arguments[0].reverse()
+// }
+
+// function swapValues(args) {
+//   var temp = args[0]
+//   args[0] = args[1]
+//   args[1] = temp
+// }
+
+// function swapValues() {
+//   var args = Array.prototype.slice.call(arguments)[0]
+//   var temp = args[0]
+//   args[0] = args[1]
+//   args[1] = temp
+// }
