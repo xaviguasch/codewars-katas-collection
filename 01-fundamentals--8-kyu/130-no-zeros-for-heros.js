dc9612f2af2274c6e4b87fe8eaddcd18a41d416e
xaviguasch <xaviguasch@gmail.com>
2022-04-13 23:17:51 +0200

    added 130-no-boring-zeros kata

---
+++ 01-fundamentals--8-kyu/130-no-zeros-for-heros.js
@@ -0,0 +1,21 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// 1450 -> 145
+// 960000 -> 96
+// 1050 -> 105
+// -1050 -> -105
+// Zero alone is fine, don't worry about it. Poor guy anyway
+
+const noBoringZeros = (n) => {
+  return
+}
+
+console.log(noBoringZeros(1450))
+console.log(noBoringZeros(9600000))
+console.log(noBoringZeros(1050))
+console.log(noBoringZeros(-105))
+console.log(noBoringZeros(0))
