fafa2e74affdc19024260a1d38f21164bd10a087
xaviguasch <xaviguasch@gmail.com>
2022-04-14 20:50:05 +0200

    added exclamation-marks-series-1-remove kata

---
+++ 01-fundamentals--8-kyu/132-exclamation-marks-series-1-remove.js
@@ -0,0 +1,21 @@
+// Description:
+// Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
+
+// Examples
+// remove("Hi!") === "Hi"
+// remove("Hi!!!") === "Hi!!"
+// remove("!Hi") === "!Hi"
+// remove("!Hi!") === "!Hi"
+// remove("Hi! Hi!") === "Hi! Hi"
+// remove("Hi") === "Hi"
+
+const remove = (string) => {
+  return ''
+}
+
+console.log(remove('Hi!'))
+console.log(remove('Hi!!!'))
+console.log(remove('!Hi'))
+console.log(remove('!Hi!'))
+console.log(remove('Hi! Hi!'))
+console.log(remove('Hi'))
