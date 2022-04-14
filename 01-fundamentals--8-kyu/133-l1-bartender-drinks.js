8bb1b5e7b7df63684650ce36780a9f55074b7254
xaviguasch <xaviguasch@gmail.com>
2022-04-14 21:38:08 +0200

    added alternative solution

--- 01-fundamentals--8-kyu/133-l1-bartender-drinks.js
+++ 01-fundamentals--8-kyu/133-l1-bartender-drinks.js
@@ -44,3 +44,16 @@ const getDrinkByProfession = (param) => {
 console.log(getDrinkByProfession('jabrOni'))
 console.log(getDrinkByProfession('scHOOl counselor'))
 console.log(getDrinkByProfession('prOgramMer'))
+
+// ALTERNATIVE SOLUTION
+
+// const drinks = {
+//   "jabroni": "Patron Tequila",
+//   "school counselor": "Anything with Alcohol",
+//   "programmer": "Hipster Craft Beer",
+//   "bike gang member": "Moonshine",
+//   "politician": "Your tax dollars",
+//   "rapper": "Cristal"
+// }
+
+// const getDrinkByProfession = profession => drinks[profession.toLowerCase()] || "Beer"
