23e72d4cdb13eea237e794bdd2fc2f9124564d12
xaviguasch <xaviguasch@gmail.com>
2022-04-15 23:30:41 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/137-grasshopper-variable-assignment-debug.js
+++ 01-fundamentals--8-kyu/137-grasshopper-variable-assignment-debug.js
@@ -1,11 +1,9 @@
 // Variable assignment
 // Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
 
+var a = 'dev'
+var b = 'Lab'
 
-var a == "dev"
-var b == "Lab"
+var name = a + b
 
-var name == a + b
-
-
-console.log(name);
+console.log(name)
