d1b73aa0e3226ec9761193b38ec778d4fa57c7d2
xaviguasch <xaviguasch@gmail.com>
2022-04-15 23:32:53 +0200

    added 138-super-duper-easy kata

---
+++ 01-fundamentals--8-kyu/138-super-duper-easy.js
@@ -0,0 +1,10 @@
+// Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+const problem = (x) => {
+  return
+}
+
+console.log(problem('hello'))
+console.log(problem(1))
+console.log(problem(5))
+console.log(problem(0))
