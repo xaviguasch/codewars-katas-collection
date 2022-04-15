0e8c4af0c9afe30c09416942b135e9e8e4d4f53e
xaviguasch <xaviguasch@gmail.com>
2022-04-15 23:43:25 +0200

    added 140-printing-array-elements-with-comma-delimiters kata

---
+++ 01-fundamentals--8-kyu/140-printing-array-elements-with-comma-delimeters.js
@@ -0,0 +1,13 @@
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+const printArray = (array) => {
+  return
+}
+
+console.log(printArray([2, 4, 5, 2]))
