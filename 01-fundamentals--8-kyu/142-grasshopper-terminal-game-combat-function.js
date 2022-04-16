c304b67c3325d6d14fd9b5848fe150ef2bc6383d
xaviguasch <xaviguasch@gmail.com>
2022-04-16 22:08:43 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/142-grasshopper-terminal-game-combat-function.js
+++ 01-fundamentals--8-kyu/142-grasshopper-terminal-game-combat-function.js
@@ -1,8 +1,6 @@
 // Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
 
-const combat = (health, damage) => {
-  return
-}
+const combat = (health, damage) => (health - damage >= 0 ? health - damage : 0)
 
 console.log(combat(100, 5))
 console.log(combat(92, 8))
