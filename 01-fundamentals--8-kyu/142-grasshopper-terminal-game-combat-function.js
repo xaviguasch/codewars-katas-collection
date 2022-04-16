3cec0eec2299efa75b8436cf3b68f142a4784a44
xaviguasch <xaviguasch@gmail.com>
2022-04-16 22:09:34 +0200

    added alternative solutions

--- 01-fundamentals--8-kyu/142-grasshopper-terminal-game-combat-function.js
+++ 01-fundamentals--8-kyu/142-grasshopper-terminal-game-combat-function.js
@@ -5,3 +5,11 @@ const combat = (health, damage) => (health - damage >= 0 ? health - damage : 0)
 console.log(combat(100, 5))
 console.log(combat(92, 8))
 console.log(combat(20, 30))
+
+// ALTERNATIVE SOLUTIONS
+
+// function combat(health, damage) {
+//   return health < damage ? 0 : health - damage
+// }
+
+// const combat = (health, damage) => Math.max(0, health - damage);
