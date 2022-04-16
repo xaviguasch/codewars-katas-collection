df40e3171756c4b9e24ff9d3be2be8574995d056
xaviguasch <xaviguasch@gmail.com>
2022-04-16 22:07:06 +0200

    added 142-grasshopper-terminal-game-combat-function kata

---
+++ 01-fundamentals--8-kyu/142-grasshopper-terminal-game-combat-function.js
@@ -0,0 +1,9 @@
+// Create a combat function that takes the player's current health and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+
+const combat = (health, damage) => {
+  return
+}
+
+console.log(combat(100, 5))
+console.log(combat(92, 8))
+console.log(combat(20, 30))
