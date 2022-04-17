8b34695543da2e539e01cb9bf673e050df557c84
xaviguasch <xaviguasch@gmail.com>
2022-04-17 23:23:57 +0200

    removed previous kata due to duplication mistake

--- 01-fundamentals--8-kyu/148-name-shuffler.js
+++
@@ -1,7 +0,0 @@
-// Write a function that returns a string in which firstname is swapped with last name.
-
-// nameShuffler('john McClane'); => "McClane john"
-
-const nameShuffler = (str) => str.split(' ').reverse().join(' ')
-
-console.log(nameShuffler('john McClane'))
