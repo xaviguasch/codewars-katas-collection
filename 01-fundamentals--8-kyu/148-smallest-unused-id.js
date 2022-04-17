dc944ac9108a8cc7f93ad19ecd24f1c1500d26e4
xaviguasch <xaviguasch@gmail.com>
2022-04-17 23:35:20 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/148-smallest-unused-id.js
+++ 01-fundamentals--8-kyu/148-smallest-unused-id.js
@@ -9,7 +9,13 @@
 // Go on and code some pure awesomeness!
 
 const nextId = (ids) => {
-  return 0
+  for (let i = 0; i < ids.length; i++) {
+    if (ids.indexOf(i) === -1) {
+      return i
+    }
+  }
+
+  return ids.length
 }
 
 console.log(nextId([0, 1, 2, 3, 5]))
