53cdd0e837046248cf65e7b3a350468f5b4d381a
xaviguasch <xaviguasch@gmail.com>
2022-04-17 23:18:50 +0200

    added 148-name-shuffler kata

---
+++ 01-fundamentals--8-kyu/148-name-shuffler.js
@@ -0,0 +1,9 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+
+const nameShuffler = (str) => {
+  return
+}
+
+console.log(nameShuffler('john McClane'))
