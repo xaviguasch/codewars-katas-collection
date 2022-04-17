20bd549b85705d8e14a05df4db6d3585a8e46c41
xaviguasch <xaviguasch@gmail.com>
2022-04-17 23:02:14 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/144-kata-example-twist.js
+++ 01-fundamentals--8-kyu/144-kata-example-twist.js
@@ -3,3 +3,9 @@
 // Add the value "codewars" to the array websites/Websites 1,000 times.
 
 var websites = []
+
+for (let i = 0; i < 1000; i++) {
+  websites.push('codewars')
+}
+
+console.log(websites.length)
