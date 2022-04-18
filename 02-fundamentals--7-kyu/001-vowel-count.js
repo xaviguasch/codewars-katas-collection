68ad107350615a9888a8d17ecec2b36e0e33409d
xaviguasch <xaviguasch@gmail.com>
2022-04-18 22:44:15 +0200

    added 001-vowel-count 7kyu kata

---
+++ 02-fundamentals--7-kyu/001-vowel-count.js
@@ -0,0 +1,13 @@
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+const getCount = (str) => {
+  let vowelsCount = 0
+
+  return vowelsCount
+}
+
+console.log(getCount('abracadabra'))
