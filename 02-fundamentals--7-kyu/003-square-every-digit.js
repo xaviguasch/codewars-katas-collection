3baedfe9917c53b7bae4b16dc1d351dc5b598cce
xaviguasch <xaviguasch@gmail.com>
2022-04-19 19:57:17 +0200

    added 7kyu 003-square-every-digit kata

---
+++ 02-fundamentals--7-kyu/003-square-every-digit.js
@@ -0,0 +1,13 @@
+// Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+// Note: The function accepts an integer and returns an integer
+
+const squareDigits = (num) => {
+  return 0
+}
+
+console.log(squareDigits(3212))
+console.log(squareDigits(2112))
+console.log(squareDigits(0))
