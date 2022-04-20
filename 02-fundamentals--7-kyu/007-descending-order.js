067a6cc2f4c5c15389f6cd3f47e94191b555535f
xaviguasch <xaviguasch@gmail.com>
2022-04-20 22:52:29 +0200

    added 7kyu 007-descending-order kata

---
+++ 02-fundamentals--7-kyu/007-descending-order.js
@@ -0,0 +1,19 @@
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+const descendingOrder = (n) => {
+  return
+}
+
+console.log(descendingOrder(0))
+console.log(descendingOrder(1))
+console.log(descendingOrder(111))
+console.log(descendingOrder(15))
+console.log(descendingOrder(1021))
+console.log(descendingOrder(123456789))
