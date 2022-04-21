1f1ce38ed66f4ff166c4bf5d200ebd8eac3947b4
xaviguasch <xaviguasch@gmail.com>
2022-04-21 20:29:33 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/008-list-filtering.js
+++ 02-fundamentals--7-kyu/008-list-filtering.js
@@ -5,9 +5,7 @@
 // filter_list([1,'a','b',0,15]) == [1,0,15]
 // filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
 
-const filter_list = (l) => {
-  return
-}
+const filter_list = (l) => l.filter((el) => typeof el !== 'string')
 
 console.log(filter_list([1, 2, 'a', 'b']))
 console.log(filter_list([1, 'a', 'b', 0, 15]))
