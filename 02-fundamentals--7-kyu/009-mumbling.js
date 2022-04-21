6d91650f3d19ab5dcdfb71dca7098fc7ab23917a
xaviguasch <xaviguasch@gmail.com>
2022-04-21 20:47:20 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/009-mumbling.js
+++ 02-fundamentals--7-kyu/009-mumbling.js
@@ -7,7 +7,18 @@
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
 
 const accum = (s) => {
-  return
+  const arr = s.split('')
+
+  let newArr = []
+
+  for (let i = 0; i < arr.length; i++) {
+    const letterLower = arr[i].toLowerCase()
+    const letterUpper = letterLower.toUpperCase()
+
+    newArr.push(letterUpper + letterLower.repeat(i))
+  }
+
+  return newArr.join('-')
 }
 
 console.log(accum('ZpglnRxqenU'))
