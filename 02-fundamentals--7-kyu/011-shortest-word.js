3bb07bbd81b07dd4b3a2a37af6daa367df89a46a
xaviguasch <xaviguasch@gmail.com>
2022-04-21 21:09:49 +0200

    added 7kyu 011-shortest-word kata

---
+++ 02-fundamentals--7-kyu/011-shortest-word.js
@@ -0,0 +1,13 @@
+// Simple, given a string of words, return the length of the shortest word(s).
+
+// String will never be empty and you do not need to account for different data types.
+
+const findShorts = (s) => {
+  return
+}
+
+console.log(findShorts('bitcoin take over the world maybe who knows perhap'))
+console.log(
+  findShorts('turns out random test cases are easier than writing out basic ones')
+)
+console.log(findShorts("Letn's travel abroad shall we"))
