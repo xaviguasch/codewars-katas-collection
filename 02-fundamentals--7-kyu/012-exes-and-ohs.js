b90f5ce1d317671645aba3fb75310bfe46044295
xaviguasch <xaviguasch@gmail.com>
2022-04-22 22:09:34 +0200

    added 7kyu 012-exes-and-ohs kata

---
+++ 02-fundamentals--7-kyu/012-exes-and-ohs.js
@@ -0,0 +1,19 @@
+// Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+const X0 = (str) => {
+  return
+}
+
+console.log(XO('xo'))
+console.log(XO('xxOo'))
+console.log(XO('xxxm'))
+console.log(XO('Oo'))
+console.log(XO('ooom'))
