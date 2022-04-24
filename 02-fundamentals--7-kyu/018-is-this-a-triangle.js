994f3d6f2faa33e2d6634bcffed448610173a76c
xaviguasch <xaviguasch@gmail.com>
2022-04-24 21:17:04 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/018-is-this-a-triangle.js
+++ 02-fundamentals--7-kyu/018-is-this-a-triangle.js
@@ -2,8 +2,14 @@
 
 // (In this case, all triangles must have surface greater than 0 to be accepted).
 
+// PERSONAL NOTE (NOT IN THE ORIGINAL KATA)
+// It can be triangle only if sum of any two sides is larger than the third one. Somehow I can't see anything difficult in this.
+
 const isTriangle = (a, b, c) => {
-  return
+  const sum = a + b + c
+  const maxSide = Math.max(a, b, c)
+
+  return sum - maxSide > maxSide
 }
 
 console.log(isTriangle(1, 2, 2))
