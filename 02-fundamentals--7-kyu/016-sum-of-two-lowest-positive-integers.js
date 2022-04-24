94add0ca38d0a2c677e8a744971e94214956487e
xaviguasch <xaviguasch@gmail.com>
2022-04-24 20:32:35 +0200

    added 7kyu 016-sum-of-two-lowest-positive-numbers kata

---
+++ 02-fundamentals--7-kyu/016-sum-of-two-lowest-positive-integers.js
@@ -0,0 +1,13 @@
+// Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+// For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+// [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+const sumTwoSmallestNumbers = (numbers) => {
+  return
+}
+
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]))
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]))
