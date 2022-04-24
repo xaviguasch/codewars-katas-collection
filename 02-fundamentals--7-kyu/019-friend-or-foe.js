44e286344687acbcfdef0ef5f03299cace46f01f
xaviguasch <xaviguasch@gmail.com>
2022-04-24 21:49:26 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/019-friend-or-foe.js
+++ 02-fundamentals--7-kyu/019-friend-or-foe.js
@@ -9,13 +9,9 @@
 // friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
 // Note: keep the original order of the names in the output.
 
-const friend = (friends) => {
-  return
-}
+const friend = (friends) => friends.filter((f) => f.length === 4)
 
 console.log(friend(['Ryan', 'Kieran', 'Mark']))
-
-console.log(friend())
 console.log(friend(['Ryan', 'Jimmy', '123', '4', 'Cool Man']))
 console.log(friend(['Jimm', 'Cari', 'aret', 'truehdnviegkwgvke', 'sixtyiscooooool']))
 console.log(friend(['Love', 'Your', 'Face', '1']))
