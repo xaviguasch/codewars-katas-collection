f6edf02386b02ca8c93c8767098bf12e08a53aaf
xaviguasch <xaviguasch@gmail.com>
2022-04-24 22:02:41 +0200

    added alternative solutions to previous kata

--- 02-fundamentals--7-kyu/020-two-to-one.js
+++ 02-fundamentals--7-kyu/020-two-to-one.js
@@ -20,3 +20,11 @@ const longest = (s1, s2) => {
 
 console.log(longest('aretheyhere', 'yestheyarehere'))
 console.log(longest('loopingisfunbutdangerous', 'lessdangerousthancoding'))
+
+// ALTERNATIVE SOLUTIONS
+
+// const longest = (s1, s2) => [...new Set(s1+s2)].sort().join('')
+
+// function longest(s1, s2) {
+//   return Array.from(new Set(s1 + s2)).sort().join('');
+// }
