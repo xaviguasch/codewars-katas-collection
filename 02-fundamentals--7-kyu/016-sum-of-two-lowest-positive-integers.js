ddf9479a145ebaf4d9c1d608a7d2e800e5324637
xaviguasch <xaviguasch@gmail.com>
2022-04-24 20:35:15 +0200

    added alternative solution to previous kata

--- 02-fundamentals--7-kyu/016-sum-of-two-lowest-positive-integers.js
+++ 02-fundamentals--7-kyu/016-sum-of-two-lowest-positive-integers.js
@@ -5,9 +5,18 @@
 // [10, 343445353, 3453445, 3453545353453] should return 3453455.
 
 const sumTwoSmallestNumbers = (numbers) => {
-  return
+  const sortedArr = numbers.sort((a, b) => a - b)
+
+  return sortedArr[0] + sortedArr[1]
 }
 
 console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]))
 console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))
 console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7]))
+
+// ALTERNATIVE SOLUTION
+
+// function sumTwoSmallestNumbers(numbers) {
+//   var [ a, b ] = numbers.sort((a, b) => a - b)
+//   return a + b
+// }
