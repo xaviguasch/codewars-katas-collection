fe9d8b4a36ecebc56467df5dfeeb5242a51c5e6d
xaviguasch <xaviguasch@gmail.com>
2022-04-25 22:26:39 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/024-printer-errors.js
+++ 02-fundamentals--7-kyu/024-printer-errors.js
@@ -28,3 +28,18 @@ const printerError = (s) => {
 }
 
 console.log(printerError('aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz'))
+
+//  ALTERNATIVE SOLUTIONS
+
+// function printerError(s) {
+//   // your code
+//   var count = 0;
+//   for(var i = 0; i < s.length; i++) {
+//     if (s[i] > "m") {
+//       count++;
+//     }
+//   }
+//   return count+"/"+s.length;
+// }
+
+// const printerError = ($) => [ ($.match(/[n-z]/g) || []).length, $.length ].join('/')
