dfc835b3916c1f1c1f5be827eaba82591c8aa668
xaviguasch <xaviguasch@gmail.com>
2022-04-25 22:13:36 +0200

    added alternative solutions to previous kata

--- 02-fundamentals--7-kyu/023-sum-of-odd-numbers.js
+++ 02-fundamentals--7-kyu/023-sum-of-odd-numbers.js
@@ -17,3 +17,33 @@ const rowSumOddNumbers = (n) => {
 
 console.log(rowSumOddNumbers(1))
 console.log(rowSumOddNumbers(42))
+
+// ALTERNATIVE SOLUTIONS
+
+// function rowSumOdd(n) {
+//   let start = n * (n - 1) + 1;
+//   let sum = 0;
+//   for (let i = 0; i < n; i++) {
+//     sum += start + (i * 2);
+//   }
+//   return sum;
+// }
+
+// function rowSumOddNumbers(n)
+// {
+//   /* The rows' start numbers are Hogben's centered polygonal numbers:
+//      1, 3, 7, 13, 21, 31, 43 = b[n] = n^2 - n + 1.
+//      <https://oeis.org/A002061>
+
+//      The sum of one row is given by:
+//      s[n] = n^2 + n(b[n] - 1).
+//      <https://www.quora.com/What-is-the-sum-of-n-consecutive-odd-integers/answer/Xavier-Dectot>
+
+//      Inline b[n]:
+//      s[n] = n^2 + n(n^2 - n + 1 - 1)
+//           = n^2 + n(n^2 - n)
+//           = n^2 + n^3 - n^2
+//           = n^3
+//      ... oh. */
+//   return n * n * n;
+// }
