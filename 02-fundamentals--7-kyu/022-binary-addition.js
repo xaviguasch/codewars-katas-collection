0ce95f93a1d46e4a5d359ef43884d2fa2e8fe957
xaviguasch <xaviguasch@gmail.com>
2022-04-25 22:06:04 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/022-binary-addition.js
+++ 02-fundamentals--7-kyu/022-binary-addition.js
@@ -8,7 +8,11 @@
 // 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
 
 const addBinary = (a, b) => {
-  return
+  const sum = a + b
+
+  return sum.toString(2)
 }
 
 console.log(addBinary(1, 2))
+console.log(addBinary(1, 1))
+console.log(addBinary(5, 9))
