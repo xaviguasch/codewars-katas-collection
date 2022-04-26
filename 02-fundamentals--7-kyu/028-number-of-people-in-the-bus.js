c304021b23538759bc222295a746bb8875249bce
xaviguasch <xaviguasch@gmail.com>
2022-04-26 22:36:25 +0200

    added alternative solutions to previous kata

--- 02-fundamentals--7-kyu/028-number-of-people-in-the-bus.js
+++ 02-fundamentals--7-kyu/028-number-of-people-in-the-bus.js
@@ -50,3 +50,22 @@ console.log(
   ])
 )
 console.log(number([[0, 0]]))
+
+// ALTERNATIVE SOLUTIONS
+
+// const number = (busStops) => busStops.reduce((rem, [on, off]) => rem + on - off, 0);
+
+// const number = busStops => busStops.reduce((p,n) => p+n[0]-n[1],0)
+
+// var number = function(busStops){
+//   var totalPeople = 0;
+//   for (var i=0; i<busStops.length; i++) {
+//     totalPeople += busStops[i][0];
+//     totalPeople -= busStops[i][1];
+//   }
+//   return totalPeople;
+// }
+
+// var number = function(busStops){
+//   return busStops.map(x => x[0] - x[1]).reduce( (x, y) => x + y);
+// }
