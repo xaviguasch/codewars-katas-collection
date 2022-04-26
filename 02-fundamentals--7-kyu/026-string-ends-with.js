75201ec88761fee47a15a8e9d6a4daeccfe3d60f
xaviguasch <xaviguasch@gmail.com>
2022-04-26 22:10:58 +0200

    added 7kyu 026-string-ends-with kata

---
+++ 02-fundamentals--7-kyu/026-string-ends-with.js
@@ -0,0 +1,13 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+const solution = (str, ending) => {
+  return
+}
+
+console.log(solution('abcde', 'cde'))
+console.log(solution('abcde', 'abc'))
