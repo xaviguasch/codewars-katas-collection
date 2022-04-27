9653b7a6880967ae588c657728a7235e75bed67a
xaviguasch <xaviguasch@gmail.com>
2022-04-28 00:01:22 +0200

    added alternative solutions to previous kata

--- 02-fundamentals--7-kyu/033-remove-the-minimum.js
+++ 02-fundamentals--7-kyu/033-remove-the-minimum.js
@@ -28,3 +28,12 @@ const removeSmallest = (numbers) => {
 console.log(removeSmallest([1, 2, 3, 4, 5]))
 console.log(removeSmallest([5, 3, 2, 1, 4]))
 console.log(removeSmallest([2, 2, 1, 2, 1]))
+
+// ALTERNATIVE SOLUTIONS
+
+// function removeSmallest(numbers) {
+//   let indexOfMin = numbers.indexOf(Math.min(...numbers));
+//   return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
+// }
+
+// const removeSmallest = numbers => numbers.filter((n,i) => i !== numbers.indexOf(Math.min(...numbers)));
