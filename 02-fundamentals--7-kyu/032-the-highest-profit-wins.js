0950c47840614df134aefb8768d67df539a9552c
xaviguasch <xaviguasch@gmail.com>
2022-04-27 23:48:19 +0200

    added alternative solution to previous kata

--- 02-fundamentals--7-kyu/032-the-highest-profit-wins.js
+++ 02-fundamentals--7-kyu/032-the-highest-profit-wins.js
@@ -21,3 +21,7 @@ const minMax = (arr) => {
 console.log(minMax([1, 2, 3, 4, 5]))
 console.log(minMax([2334454, 5]))
 console.log(minMax([1]))
+
+// ALTERNATIVE SOLUTION
+
+// const minMax = arr => [ Math.min(...arr), Math.max(...arr) ];
