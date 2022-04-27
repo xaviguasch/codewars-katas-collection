967dc2db5475367e92be25b8c57ae2514dd35d34
xaviguasch <xaviguasch@gmail.com>
2022-04-28 00:15:44 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/034-find-the-stray-number.js
+++ 02-fundamentals--7-kyu/034-find-the-stray-number.js
@@ -9,7 +9,22 @@
 // [17, 17, 3, 17, 17, 17, 17] ==> 3
 
 const stray = (numbers) => {
-  return
+  let groupOne = [numbers[0]]
+  let groupTwo = []
+
+  // We start at index 1 cause index 0 is already inside groupOne
+  for (let i = 1; i < numbers.length; i++) {
+    numbers[i] === groupOne[0] ? groupOne.push(numbers[i]) : groupTwo.push(numbers[i])
+  }
+
+  console.log(groupOne)
+  console.log(groupTwo)
+
+  if (groupOne.length > 1) {
+    return groupTwo[0]
+  } else {
+    return groupOne[0]
+  }
 }
 
 console.log(stray([1, 2, 2]))
