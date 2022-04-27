cc8c90f5013813317f22aa122390980c2af0afbd
xaviguasch <xaviguasch@gmail.com>
2022-04-27 23:20:13 +0200

    added 7kyu 031-sum-of-the-first-nth-term-of-series kata

---
+++ 02-fundamentals--7-kyu/031-sum-of-the-first-nth-term-of-series.js
@@ -0,0 +1,23 @@
+// Your task is to write a function which returns the sum of following series upto nth term(parameter).
+
+// Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
+// Rules:
+// You need to round the answer to 2 decimal places and return it as String.
+
+// If the given value is 0 then it should return 0.00
+
+// You will only be given Natural Numbers as arguments.
+
+// Examples:(Input --> Output)
+// 1 --> 1 --> "1.00"
+// 2 --> 1 + 1/4 --> "1.25"
+// 5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"
+
+const seriesSum = (n) => {
+  return
+}
+
+console.log(seriesSum(1))
+console.log(seriesSum(2))
+console.log(seriesSum(3))
+console.log(seriesSum(4))
