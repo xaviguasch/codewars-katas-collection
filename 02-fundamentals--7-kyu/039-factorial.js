2ec6adc019e88d22e77d5ab8d66f2f5473136bc0
xaviguasch <xaviguasch@gmail.com>
2022-04-28 22:33:45 +0200

    added alternative solution to previous kata

--- 02-fundamentals--7-kyu/039-factorial.js
+++ 02-fundamentals--7-kyu/039-factorial.js
@@ -20,3 +20,10 @@ console.log(factorial(0))
 console.log(factorial(1))
 console.log(factorial(2))
 console.log(factorial(3))
+
+// ALTERNATIVE SOLUTION
+
+function factorial(n) {
+  if (n < 0 || n > 12) throw new RangeError()
+  return n <= 1 ? 1 : n * factorial(n - 1)
+}
