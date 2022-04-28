3e20b12d4b5a23fd2abebade46000266b9b09f5a
xaviguasch <xaviguasch@gmail.com>
2022-04-28 22:18:51 +0200

    added 7kyu 037-sort-numbers kata

---
+++ 02-fundamentals--7-kyu/037-sort-numbers.js
@@ -0,0 +1,16 @@
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+const solution = (nums) => {
+  return
+}
+
+console.log(solution([1, 2, 3, 10, 5]))
+console.log(solution(null))
+console.log(solution([]))
+console.log(solution([20, 2, 10]))
+console.log(solution([2, 20, 10]))
