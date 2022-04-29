5a1e5781124a08db80e84d9affc51326243beb99
xaviguasch <xaviguasch@gmail.com>
2022-04-29 22:23:48 +0200

    added 7kyu 041-remove-anchor-from-url kata

---
+++ 02-fundamentals--7-kyu/041-remove-anchor-from-url.js
@@ -0,0 +1,13 @@
+// Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+// Examples
+// "www.codewars.com#about" --> "www.codewars.com"
+// "www.codewars.com?page=1" -->"www.codewars.com?page=1"
+
+const removeUrlAnchor = (url) => {
+  return
+}
+
+console.log(removeUrlAnchor('www.codewars.com#about'))
+console.log(removeUrlAnchor('www.codewars.com/katas/?page=1#about'))
+console.log(removeUrlAnchor('www.codewars.com/katas/'))
