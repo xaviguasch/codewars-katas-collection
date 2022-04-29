cba2f0e2c28bc83d303834f0c46ee866f9a318ce
xaviguasch <xaviguasch@gmail.com>
2022-04-29 22:31:38 +0200

    added alternative solutions to previous kata

--- 02-fundamentals--7-kyu/041-remove-anchor-from-url.js
+++ 02-fundamentals--7-kyu/041-remove-anchor-from-url.js
@@ -20,3 +20,25 @@ const removeUrlAnchor = (url) => {
 console.log(removeUrlAnchor('www.codewars.com#about'))
 console.log(removeUrlAnchor('www.codewars.com/katas/?page=1#about'))
 console.log(removeUrlAnchor('www.codewars.com/katas/'))
+
+// ALTERNATIVE SOLUTIONS
+
+// function removeUrlAnchor(url) {
+//   return url.split('#')[0]
+// }
+
+// function removeUrlAnchor(url) {
+//   let string = ''
+//   for (let i = 0; i < url.length; i++) {
+//     if (url[i] === '#') {
+//       break
+//     }
+//     string += url[i]
+//   }
+//   return string
+// }
+
+// function removeUrlAnchor(url) {
+//   var index = url.indexOf('#')
+//   return index == -1 ? url : url.substring(0, url.indexOf('#'))
+// }
