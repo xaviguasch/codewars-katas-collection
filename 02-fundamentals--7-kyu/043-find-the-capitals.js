633dcbab6119b818e6d6c5b99c761252a711c5ef
xaviguasch <xaviguasch@gmail.com>
2022-04-30 23:28:51 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/043-find-the-capitals.js
+++ 02-fundamentals--7-kyu/043-find-the-capitals.js
@@ -1,7 +1,10 @@
 // Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
 
 const capitals = (word) => {
-  return
+  return word
+    .split('')
+    .map((letter) => (letter.toUpperCase() === letter ? word.indexOf(letter) : null))
+    .filter((el) => el !== null)
 }
 
 console.log(capitals('CodEWaRs'))
