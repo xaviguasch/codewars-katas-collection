729f90b10193da121813b9079b1adb815cf082eb
xaviguasch <xaviguasch@gmail.com>
2022-04-30 23:36:40 +0200

    added alternative solution to previous kata

--- 02-fundamentals--7-kyu/043-find-the-capitals.js
+++ 02-fundamentals--7-kyu/043-find-the-capitals.js
@@ -8,3 +8,11 @@ const capitals = (word) => {
 }
 
 console.log(capitals('CodEWaRs'))
+
+// ALTERNATIVE SOLUTION
+
+// var capitals = function (word) {
+//   return word.split('').reduce(function (memo, v, i) {
+//     return v === v.toUpperCase() ? memo.concat(i) : memo
+//   }, [])
+// }
