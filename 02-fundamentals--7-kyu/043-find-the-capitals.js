e5243f899c9c4eada722f03ca265aff483307038
xaviguasch <xaviguasch@gmail.com>
2022-04-30 23:25:11 +0200

    added 7kyu 043-find-the-capitals kata

---
+++ 02-fundamentals--7-kyu/043-find-the-capitals.js
@@ -0,0 +1,7 @@
+// Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+const capitals = (word) => {
+  return
+}
+
+console.log(capitals('CodEWaRs'))
