5f1b8436bf01b197ef511180e7d73c3d6dd89750
xaviguasch <xaviguasch@gmail.com>
2022-05-01 21:46:05 +0200

    added alternative solution to previous kata

--- 02-fundamentals--7-kyu/046-two-oldest-ages.js
+++ 02-fundamentals--7-kyu/046-two-oldest-ages.js
@@ -14,3 +14,7 @@ const twoOldestAges = (ages) => {
 
 console.log(twoOldestAges([1, 5, 87, 45, 8, 8]))
 console.log(twoOldestAges([6, 5, 83, 5, 3, 18]))
+
+// ALTERNATIVE SOLUTION
+
+// const twoOldestAges = (ages) => ages.sort((a, b) => a - b).slice(-2)
