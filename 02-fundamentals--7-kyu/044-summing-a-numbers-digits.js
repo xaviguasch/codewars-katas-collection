e0a3c2685f795d1c9dda76e0f1f80a53fba8858f
xaviguasch <xaviguasch@gmail.com>
2022-05-01 21:37:11 +0200

    added alternative solution to previous kata

--- 02-fundamentals--7-kyu/044-summing-a-numbers-digits.js
+++ 02-fundamentals--7-kyu/044-summing-a-numbers-digits.js
@@ -19,3 +19,14 @@ const sumDigits = (number) => {
 console.log(sumDigits(10))
 console.log(sumDigits(99))
 console.log(sumDigits(-32))
+
+// ALTERNATIVE SOLUTION
+
+// function sumDigits(number) {
+//   return Math.abs(number)
+//     .toString()
+//     .split('')
+//     .reduce(function (a, b) {
+//       return +a + +b
+//     }, 0)
+// }
