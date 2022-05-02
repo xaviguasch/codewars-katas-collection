6e1178e71fb406c80cd832fcbf000ffa77785c4d
xaviguasch <xaviguasch@gmail.com>
2022-05-02 22:20:04 +0200

    added 7kyu 047-sum-all-the-multiples-of-3-and-5 kata

---
+++ 02-fundamentals--7-kyu/047-sum-all-the-multiples-of-3-and-5.js
@@ -0,0 +1,16 @@
+// Your task is to write function findSum.
+
+// Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+// For example:
+
+// findSum(5) should return 8 (3 + 5)
+
+// findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+const findSum = (n) => {
+  return
+}
+
+console.log(findSum(5))
+console.log(findSum(10))
