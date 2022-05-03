e82dfd0b13a7b59c303c23b80570d580143f3558
xaviguasch <xaviguasch@gmail.com>
2022-05-03 19:28:57 +0200

    added alternative solution to previous kata

--- 02-fundamentals--7-kyu/051-small-enough.js
+++ 02-fundamentals--7-kyu/051-small-enough.js
@@ -2,9 +2,13 @@
 
 // You can assume all values in the array are numbers.
 
-const smallEnough = (a, limit) => {
-  return
-}
+const smallEnough = (a, limit) => a.every((val) => val <= limit)
 
 console.log(smallEnough([66, 101], 200))
 console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100))
+
+// ALTERNATIVE SOLUTION
+
+function smallEnough(a, limit) {
+  return Math.max(...a) <= limit
+}
