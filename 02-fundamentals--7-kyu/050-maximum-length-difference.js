8babe7a6ae1cc3d378a86de1218faffa9caa7af0
xaviguasch <xaviguasch@gmail.com>
2022-05-03 19:23:13 +0200

    added alternative solution to previous kata

--- 02-fundamentals--7-kyu/050-maximum-length-difference.js
+++ 02-fundamentals--7-kyu/050-maximum-length-difference.js
@@ -41,3 +41,12 @@ var s1 = [
 ]
 var s2 = ['cccooommaaqqoxii', 'gggqaffhhh', 'tttoowwwmmww']
 console.log(mxdiflg(s1, s2))
+
+// ALTERNATIVE SOLUTION
+
+// function mxdiflg(a1, a2) {
+//   if (a1.length === 0 || a2.length === 0) return -1
+//   let l1 = a1.map(str => str.length)
+//   let l2 = a2.map(str => str.length)
+//   return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
+// }
