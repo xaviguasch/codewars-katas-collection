c12eaaddaac117157ca4a668771b26a4833783ec
xaviguasch <xaviguasch@gmail.com>
2022-05-03 19:25:30 +0200

    added 7kyu 051-small-enough kata

---
+++ 02-fundamentals--7-kyu/051-small-enough.js
@@ -0,0 +1,10 @@
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+const smallEnough = (a, limit) => {
+  return
+}
+
+console.log(smallEnough([66, 101], 200))
+console.log(smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100))
