379328b1441f2e692dda920d3fa421a12d302172
xaviguasch <xaviguasch@gmail.com>
2022-05-04 23:36:12 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/053-simple-fun-176-reverse-letter.js
+++ 02-fundamentals--7-kyu/053-simple-fun-176-reverse-letter.js
@@ -13,7 +13,40 @@
 // [output] a string
 
 const reverseLetter = (str) => {
-  return
+  const alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ]
+
+  return str
+    .split('')
+    .reverse()
+    .filter((char) => alphabet.indexOf(char) !== -1)
+    .join('')
 }
 
 console.log(reverseLetter('krishan'))
