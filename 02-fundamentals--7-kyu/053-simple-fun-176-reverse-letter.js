21f53a138bd0f7ea0dfc9d08928c4744446684d8
xaviguasch <xaviguasch@gmail.com>
2022-05-04 23:33:20 +0200

    added 7kyu 053-simple-fun-176-reverse-letter kata

---
+++ 02-fundamentals--7-kyu/053-simple-fun-176-reverse-letter.js
@@ -0,0 +1,22 @@
+// Task
+// Given a string str, reverse it omitting all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+const reverseLetter = (str) => {
+  return
+}
+
+console.log(reverseLetter('krishan'))
+console.log(reverseLetter('ultr53o?n'))
+console.log(reverseLetter('ab23c'))
+console.log(reverseLetter('krish21an'))
