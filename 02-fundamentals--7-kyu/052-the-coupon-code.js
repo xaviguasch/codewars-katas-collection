1e21b524724afc4c59740ce89f635b0692947e7b
xaviguasch <xaviguasch@gmail.com>
2022-05-04 23:28:32 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/052-the-coupon-code.js
+++ 02-fundamentals--7-kyu/052-the-coupon-code.js
@@ -11,8 +11,10 @@
 // checkCoupon("123", "123", "July 9, 2015", "July 9, 2015")  ===  true
 // checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
 
-const checkCoupon = (enterCode, correctCode, currentDate, expirationDate) => {
-  return
+const checkCoupon = (enteredCode, correctCode, currentDate, expirationDate) => {
+  return (
+    enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
+  )
 }
 
 console.log(checkCoupon('123', '123', 'September 5, 2014', 'October 1, 2014'))
