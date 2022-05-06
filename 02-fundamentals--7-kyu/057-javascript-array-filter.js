1d830369f1cb727d7c0bbb2d84e180f337e573d7
xaviguasch <xaviguasch@gmail.com>
2022-05-06 21:20:26 +0200

    added 7kyu 057-javascript-array-filter kata

---
+++ 02-fundamentals--7-kyu/057-javascript-array-filter.js
@@ -0,0 +1,14 @@
+// JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+const getEvenNumbers = (numbersArray) => {
+  return
+}
+
+console.log(getEvenNumbers([1, 2, 3, 6, 8, 10]))
+console.log(getEvenNumbers([1, 2]))
+console.log(getEvenNumbers([12, 14, 15]))
+console.log(getEvenNumbers([13, 15]))
