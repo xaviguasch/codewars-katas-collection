46bba554d841954093f765a03eb96f51d9f8f8ff
xaviguasch <xaviguasch@gmail.com>
2022-05-06 21:21:49 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/057-javascript-array-filter.js
+++ 02-fundamentals--7-kyu/057-javascript-array-filter.js
@@ -4,9 +4,7 @@
 
 // getEvenNumbers([2,4,5,6]) // should == [2,4,6]
 
-const getEvenNumbers = (numbersArray) => {
-  return
-}
+const getEvenNumbers = (numbersArray) => numbersArray.filter((num) => num % 2 === 0)
 
 console.log(getEvenNumbers([1, 2, 3, 6, 8, 10]))
 console.log(getEvenNumbers([1, 2]))
