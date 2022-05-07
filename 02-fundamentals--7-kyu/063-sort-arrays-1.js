af5db91988a56e2debdb6650185e4e10f4ac1e27
xaviguasch <xaviguasch@gmail.com>
2022-05-07 17:01:07 +0200

    added 7kyu 063-sort-arrays-1 kata

---
+++ 02-fundamentals--7-kyu/063-sort-arrays-1.js
@@ -0,0 +1,7 @@
+// Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
+
+const sortme = (names) => {
+  return
+}
+
+console.log(sortme(['one', 'two', 'three']))
