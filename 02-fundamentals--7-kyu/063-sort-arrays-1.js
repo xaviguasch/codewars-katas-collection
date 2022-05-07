5d5ab8a8860156a7ce44f43968ce3016a6f743b6
xaviguasch <xaviguasch@gmail.com>
2022-05-07 17:05:54 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/063-sort-arrays-1.js
+++ 02-fundamentals--7-kyu/063-sort-arrays-1.js
@@ -1,7 +1,5 @@
 // Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
 
-const sortme = (names) => {
-  return
-}
+const sortme = (names) => names.sort()
 
 console.log(sortme(['one', 'two', 'three']))
