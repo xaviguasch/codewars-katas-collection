e132b92a35201e21976c6f853997649a26c4d90a
xaviguasch <xaviguasch@gmail.com>
2022-05-07 16:44:29 +0200

    added 7kyu 061-alternate-capitalization kata

---
+++ 02-fundamentals--7-kyu/061-alternate-capitalization.js
@@ -0,0 +1,16 @@
+// Given a string, capitalize the letters that occupy even indexes and odd indexes separately, and return as shown below. Index 0 will be considered even.
+
+// For example, capitalize("abcdef") = ['AbCdEf', 'aBcDeF']. See test cases for more examples.
+
+// The input will be a lowercase string with no spaces.
+
+// Good luck!
+
+const capitalize = (s) => {
+  return
+}
+
+console.log(capitalize('abcdef'))
+console.log(capitalize('codewars'))
+console.log(capitalize('abracadabra'))
+console.log(capitalize('codewarriors'))
