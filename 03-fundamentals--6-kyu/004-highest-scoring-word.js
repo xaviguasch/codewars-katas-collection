f724d7f9a4eb7bd73b6d9001dde2e030a90966c1
xaviguasch <xaviguasch@gmail.com>
2022-05-10 23:04:52 +0200

    added 6kyu 004-highest-scoring-word kata

---
+++ 03-fundamentals--6-kyu/004-highest-scoring-word.js
@@ -0,0 +1,19 @@
+// Given a string of words, you need to find the highest scoring word.
+
+// Each letter of a word scores points according to its position in the alphabet: a = 1, b = 2, c = 3 etc.
+
+// You need to return the highest scoring word as a string.
+
+// If two words score the same, return the word that appears earliest in the original string.
+
+// All letters will be lowercase and all inputs will be valid.
+
+const high = (x) => {
+  return
+}
+
+console.log(high('man i need a taxi up to ubud'))
+console.log(high('what time are we climbing up the volcano'))
+console.log(high('take me to semynak'))
+console.log(high('aa b'))
+console.log(high('b aa'))
