3da718fbcd62cce886597f8740e93dddf3d654ab
xaviguasch <xaviguasch@gmail.com>
2022-05-11 22:57:40 +0200

    added alternative solution to previous kata

--- 03-fundamentals--6-kyu/005-find-the-unique-number.js
+++ 03-fundamentals--6-kyu/005-find-the-unique-number.js
@@ -21,7 +21,7 @@ const findUniq = (arr) => {
     }
   }
 
-  return unique
+  return Number(unique)
 }
 
 console.log(findUniq([1, 0, 0]))
@@ -30,3 +30,9 @@ console.log(findUniq([0, 0, 1]))
 console.log(findUniq([1, 1, 1, 2, 1, 1]))
 console.log(findUniq([1, 1, 2, 1, 1]))
 console.log(findUniq([3, 10, 3, 3, 3]))
+
+// ALTERNATIVE SOLUTION
+
+// function findUniq(arr) {
+//   return arr.find((n) => arr.indexOf(n) === arr.lastIndexOf(n))
+// }
