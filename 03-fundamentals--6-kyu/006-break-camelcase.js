e0367633ae0193467898ceca832f19b3296358ac
xaviguasch <xaviguasch@gmail.com>
2022-05-12 20:24:56 +0200

    added 6kyu 006-break-camel-case kata

---
+++ 03-fundamentals--6-kyu/006-break-camelcase.js
@@ -0,0 +1,15 @@
+// Complete the solution so that the function will break up camel casing, using a space between words.
+
+// Example
+// "camelCasing"  =>  "camel Casing"
+// "identifier"   =>  "identifier"
+// ""             =>  ""
+
+const solution = (string) => {
+  return
+}
+
+console.log(solution('camelCasing'))
+console.log(solution('camelCasingTest'))
+console.log(solution('identifier'))
+console.log(solution(''))
