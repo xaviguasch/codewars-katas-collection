5b7f58705d1f3c3014b820e91ab5244f6fbd95aa
xaviguasch <xaviguasch@gmail.com>
2022-05-12 20:43:22 +0200

    added solution to previous kata

--- 03-fundamentals--6-kyu/006-break-camelcase.js
+++ 03-fundamentals--6-kyu/006-break-camelcase.js
@@ -6,10 +6,28 @@
 // ""             =>  ""
 
 const solution = (string) => {
-  return
+  const arrOfLetters = string.split('')
+
+  let upperCaseIdxArr = []
+
+  arrOfLetters.map((l, idx) => {
+    if (l === l.toUpperCase()) {
+      upperCaseIdxArr.push(idx)
+    }
+
+    return l
+  })
+
+  if (upperCaseIdxArr.length !== 0) {
+    // newIdx is added because with each iteration we make the array longer
+    upperCaseIdxArr.forEach((idx, newIdx) => arrOfLetters.splice(idx + newIdx, 0, ' '))
+  }
+
+  return arrOfLetters.join('')
 }
 
 console.log(solution('camelCasing'))
 console.log(solution('camelCasingTest'))
 console.log(solution('identifier'))
 console.log(solution(''))
+console.log(solution('bigManInCampus'))
