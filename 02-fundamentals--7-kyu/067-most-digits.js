c33bbbb61ec60c98a38c36e49d50d165638aa240
xaviguasch <xaviguasch@gmail.com>
2022-05-13 21:29:28 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/067-most-digits.js
+++ 02-fundamentals--7-kyu/067-most-digits.js
@@ -2,11 +2,17 @@
 
 // If two numbers in the argument array have the same number of digits, return the first one in the array.
 
+const numberLength = (number) => `${number}`.length
+
 const findLongest = (array) => {
-  // correct title
-  return
+  const numberLengths = array.map((number) => numberLength(number))
+  const biggestLength = Math.max(...numberLengths)
+  const firstBiggest = array.find((num) => numberLength(num) === biggestLength)
+  return firstBiggest
 }
 
 console.log(findLongest([1, 10, 100]))
 console.log(findLongest([9000, 8, 800]))
 console.log(findLongest([8, 900, 500]))
+
+console.log(numberLength(10))
