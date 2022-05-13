77d888b986cf4149f5465abd99036b43d2a2fd69
xaviguasch <xaviguasch@gmail.com>
2022-05-13 21:13:54 +0200

    added 7kyu 066-find-longest kata

---
+++ 02-fundamentals--7-kyu/067-most-digits.js
@@ -0,0 +1,11 @@
+// Find the number with the most digits.
+
+// If two numbers in the argument array have the same number of digits, return the first one in the array.
+
+const findLongest = (array) => {
+  return
+}
+
+console.log(findLongest([1, 10, 100]))
+console.log(findLongest([9000, 8, 800]))
+console.log(findLongest([8, 900, 500]))
