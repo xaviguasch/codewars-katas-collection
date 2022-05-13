b8df4c98859fb8031b2e8c1cb2c57bac6bbc0738
xaviguasch <xaviguasch@gmail.com>
2022-05-13 21:31:44 +0200

    added alternative solutions to previous kata

--- 02-fundamentals--7-kyu/067-most-digits.js
+++ 02-fundamentals--7-kyu/067-most-digits.js
@@ -16,3 +16,26 @@ console.log(findLongest([9000, 8, 800]))
 console.log(findLongest([8, 900, 500]))
 
 console.log(numberLength(10))
+
+// ALTERNATIVE SOLUTIONS
+
+// const findLongest = (l) => l.reduce((a, b) => (`${b}`.length > `${a}`.length ? b : a))
+
+// function findLongest(array) {
+//   return array.reduce((res, curr) =>
+//     String(res).length < String(curr).length ? curr : res
+//   )
+// }
+
+// function findLongest(array) {
+//   let num = '0'
+//   for (let i = 0; i < array.length; i++) {
+//     let string = array[i].toString()
+//     if (string.length > num.length) {
+//       num = string
+//     }
+//   }
+//   return parseInt(num)
+// }
+
+// findLongest = (arr) => arr.sort((a, b) => (b + '').length - (a + '').length)[0]
