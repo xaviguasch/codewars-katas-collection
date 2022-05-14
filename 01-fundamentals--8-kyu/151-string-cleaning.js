0bf0506fe91f77fd67a069c4650f5ae2aa88436d
xaviguasch <xaviguasch@gmail.com>
2022-05-14 20:30:52 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/151-string-cleaning.js
+++ 01-fundamentals--8-kyu/151-string-cleaning.js
@@ -7,7 +7,10 @@
 // Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
 
 const stringClean = (s) => {
-  return
+  return s
+    .split('')
+    .filter((char) => !/\d/.test(char))
+    .join('')
 }
 
 console.log(stringClean(''))
