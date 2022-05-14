877fa1d3a7e5be3677b0c6f40f5d63d55a227034
xaviguasch <xaviguasch@gmail.com>
2022-05-14 20:32:30 +0200

    added alternative solutions to previous kata

--- 01-fundamentals--8-kyu/151-string-cleaning.js
+++ 01-fundamentals--8-kyu/151-string-cleaning.js
@@ -19,3 +19,20 @@ console.log(stringClean('123456789'))
 console.log(stringClean('(E3at m2e2!!)'))
 console.log(stringClean('Dsa32 cdsc34232 csa!!! 1I 4Am cool!'))
 console.log(stringClean('A1 A1! AAA   3J4K5L@!!!"), "A A! AAA   JKL@!!!'))
+
+// ALTERNATIVE SOLUTIONS
+
+// function stringClean(s) {
+//   return s
+//     .split('')
+//     .filter((n) => n != parseInt(n))
+//     .join('')
+// }
+
+// function stringClean(s) {
+//   return s.replace(/\d/g, '')
+// }
+
+// function stringClean(s) {
+//   return s.replace(/[0-9]/g, '')
+// }
