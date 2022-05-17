140db7aea00a0f82a2cfd7a06f5d3aa99aba8a42
xaviguasch <xaviguasch@gmail.com>
2022-05-17 20:31:18 +0200

    added alternative solution to previous kata

--- 02-fundamentals--7-kyu/069-even-numbers-in-an-array.js
+++ 02-fundamentals--7-kyu/069-even-numbers-in-an-array.js
@@ -15,3 +15,7 @@ const evenNumbers = (array, number) => {
 console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3))
 console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2))
 console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1))
+
+// ALTERNATIVE SOLUTION
+
+// const evenNumbers = (a, n) => a.filter(e => !(e % 2)).slice(-n);
