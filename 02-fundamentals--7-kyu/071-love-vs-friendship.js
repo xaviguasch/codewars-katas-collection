6c2d0d377134df3259dfa09c4fa5691b4f26948c
xaviguasch <xaviguasch@gmail.com>
2022-05-18 20:17:31 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/071-love-vs-friendship.js
+++ 02-fundamentals--7-kyu/071-love-vs-friendship.js
@@ -9,7 +9,66 @@
 // The input will always be in lowercase and never be empty.
 
 const wordsToMarks = (string) => {
-  return
+  const letters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ]
+
+  return string
+    .toLowerCase()
+    .split('')
+    .map((char) => letters.indexOf(char) + 1)
+    .reduce((acc, currV) => acc + currV, 0)
 }
 
 console.log(wordsToMarks('attitude'))
