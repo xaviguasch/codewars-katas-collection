bc44671162f3266f8a2b923eb79bccb27247bdc5
xaviguasch <xaviguasch@gmail.com>
2022-05-18 20:04:56 +0200

    added 7kyu kata 070-sum-of-all-arguments kata

---
+++ 02-fundamentals--7-kyu/070-sum-of-all-arguments.js
@@ -0,0 +1,17 @@
+// Write a function that finds the sum of all its arguments.
+
+// eg:
+
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+
+const sum = () => {
+  return
+}
+
+console.log(sum(1))
+console.log(sum(1, 2))
+console.log(sum(5, 7, 9))
+console.log(sum(12, 1, 1, 1, 1))
+console.log(sum(12, 1, 1, 1, 1, 1, 1))
