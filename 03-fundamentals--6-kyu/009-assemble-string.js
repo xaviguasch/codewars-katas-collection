14a7f24e92b438862ad3dc61731fa48b2c83b71a
xaviguasch <xaviguasch@gmail.com>
2022-05-19 20:47:11 +0200

    added alternative solutions to previous kata

--- 03-fundamentals--6-kyu/009-assemble-string.js
+++ 03-fundamentals--6-kyu/009-assemble-string.js
@@ -64,3 +64,64 @@ console.log(
     '*#*#*#*#*#*#*#*',
   ])
 )
+
+// ALTERNATIVE SOLUTIONS
+
+// function assembleString(array) {
+//   return !array.length
+//     ? ''
+//     : [...array[0]].map((x, i) => {
+//         let s = array.find((y) => y[i] != '*')
+//         return !s ? '#' : s[i]
+//       }).join``
+// }
+
+// const assembleString = (a) => {
+//   return [...Array((a[0] || []).length).keys()]
+//     .map((i) => a.map((s) => s[i]).filter((c) => c != '*')[0] || '#')
+//     .join('')
+// }
+
+// function assembleString(array) {
+//   // check to see if array has elements or is different than undefined
+//   if (array.length == 0) {
+//     array = ['']
+//   }
+
+//   // creates a list of the letters of the first element in the array
+//   let x = array[0].split('')
+
+//   // loops through all elements of the array checking for the * char on each position and replacing it
+//   // if it exists
+//   let result = array.map((e) => {
+//     for (let i = 0; i < e.length; i++) {
+//       if (x[i] == '*' && e[i] !== '*') {
+//         // replace x element at i position with e element at i position
+//         x[i] = e[i]
+//       }
+//     }
+//   })
+
+//   // if the final list still contains * changes the * to the #
+//   let answer = x.map((e) => {
+//     if (e == '*') {
+//       e = '#'
+//     }
+//     return e
+//   })
+
+//   // turns final list into string
+//   answer = answer.join('')
+
+//   return answer
+// }
+
+// const assembleString = (arr) => {
+//   const result = []
+
+//   arr.map((str) => {
+//     str.split('').map((char, i) => (result[i] = char === '*' ? result[i] || '#' : char))
+//   })
+
+//   return result.join('')
+// }
