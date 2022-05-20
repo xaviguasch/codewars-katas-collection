aee75fc4ca38fdf366de0447402380eaf8cbd3a5
xaviguasch <xaviguasch@gmail.com>
2022-05-20 16:39:20 +0200

    added alternative solutions to previous kata

--- 01-fundamentals--8-kyu/153-define-a-card-suit.js
+++ 01-fundamentals--8-kyu/153-define-a-card-suit.js
@@ -18,3 +18,22 @@ console.log(defineSuit('3♣'))
 console.log(defineSuit('3♦'))
 console.log(defineSuit('3♥'))
 console.log(defineSuit('3♠'))
+
+// ALTERNATIVE SOLUTIONS
+
+// function defineSuit(card) {
+//   const s = {
+//     '♣': 'clubs',
+//     '♠': 'spades',
+//     '♦': 'diamonds',
+//     '♥': 'hearts',
+//   }
+//   return s[card.charAt(card.length - 1)]
+// }
+
+// function defineSuit(card) {
+//   if (card.includes('♥')) return 'hearts'
+//   if (card.includes('♦')) return 'diamonds'
+//   if (card.includes('♣')) return 'clubs'
+//   if (card.includes('♠')) return 'spades'
+// }
