1957334f3ee62ba22f487e4cbd763f7a312194c7
xaviguasch <xaviguasch@gmail.com>
2022-05-21 19:30:24 +0200

    added 8kyu 157-no-loops-2-you-only-need-me kata

---
+++ 01-fundamentals--8-kyu/157-no-loops-2-you-only-need-me.js
@@ -0,0 +1,14 @@
+// *** No Loops Allowed ***
+
+// You will be given an array (a) and a value (x). All you need to do is check whether the provided array contains the value, without using a loop.
+
+// Array can contain numbers or strings. X can be either. Return true if the array contains the value, false if not. With strings you will need to account for case.
+
+const check = (a, x) => {
+  return
+}
+
+console.log(check([66, 101], 66))
+console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45))
+console.log(check(['t', 'e', 's', 't'], 'e'))
+console.log(check(['what', 'a', 'great', 'kata'], 'kat'))
