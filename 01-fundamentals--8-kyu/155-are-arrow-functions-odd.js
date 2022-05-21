958f5f31c91380b8e981d7eaebea5483ff14f8ef
xaviguasch <xaviguasch@gmail.com>
2022-05-21 19:11:02 +0200

    added 8kyu 155-are-arrow-functions-odd kata

---
+++ 01-fundamentals--8-kyu/155-are-arrow-functions-odd.js
@@ -0,0 +1,13 @@
+// Time to test your basic knowledge in functions! Return the odds from a list:
+
+// [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+// [2, 4, 6]        -->  []
+
+const odds = (values) => {
+  return
+}
+
+console.log(odds([]))
+console.log(odds([2, 4, 6]))
+console.log(odds([1, 3, 5]))
+console.log(odds([1, 2, 3, 4, 5, 6]))
