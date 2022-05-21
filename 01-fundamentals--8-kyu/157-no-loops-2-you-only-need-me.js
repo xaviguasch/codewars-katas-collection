69b71d6d183feeece077c87dc2a69e281ad1db64
xaviguasch <xaviguasch@gmail.com>
2022-05-21 19:34:00 +0200

    added alternative solutions to previous kata

--- 01-fundamentals--8-kyu/157-no-loops-2-you-only-need-me.js
+++ 01-fundamentals--8-kyu/157-no-loops-2-you-only-need-me.js
@@ -10,3 +10,9 @@ console.log(check([66, 101], 66))
 console.log(check([80, 117, 115, 104, 45, 85, 112, 115], 45))
 console.log(check(['t', 'e', 's', 't'], 'e'))
 console.log(check(['what', 'a', 'great', 'kata'], 'kat'))
+
+// ALTERNATIVE SOLUTIONS
+
+// check = (a, x) => a.some((v) => v == x)
+
+// check = (a, x) => a.indexOf(x) >= 0
