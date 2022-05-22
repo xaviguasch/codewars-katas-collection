f34668fe1e32ddc09d45bbbb4613b7228e738fe8
xaviguasch <xaviguasch@gmail.com>
2022-05-22 16:17:29 +0200

    added alternative solutions to previous kata

--- 01-fundamentals--8-kyu/158-return-the-day.js
+++ 01-fundamentals--8-kyu/158-return-the-day.js
@@ -30,3 +30,52 @@ console.log(whatDay(2))
 console.log(whatDay(3))
 console.log(whatDay(8))
 console.log(whatDay(20))
+
+// ALTERNATIVE SOLUTIONS
+
+// function whatday(num) {
+//   switch (num) {
+//     case 1:
+//       return 'Sunday'
+//     case 2:
+//       return 'Monday'
+//     case 3:
+//       return 'Tuesday'
+//     case 4:
+//       return 'Wednesday'
+//     case 5:
+//       return 'Thursday'
+//     case 6:
+//       return 'Friday'
+//     case 7:
+//       return 'Saturday'
+//     default:
+//       return 'Wrong, please enter a number between 1 and 7'
+//   }
+// }
+
+// function whatday(num) {
+//   var days = [
+//     'Sunday',
+//     'Monday',
+//     'Tuesday',
+//     'Wednesday',
+//     'Thursday',
+//     'Friday',
+//     'Saturday',
+//   ]
+//   return days[num - 1] || 'Wrong, please enter a number between 1 and 7'
+// }
+
+// function whatday(num) {
+//   let days = {
+//     1: 'Sunday',
+//     2: 'Monday',
+//     3: 'Tuesday',
+//     4: 'Wednesday',
+//     5: 'Thursday',
+//     6: 'Friday',
+//     7: 'Saturday',
+//   }
+//   return days[num] || 'Wrong, please enter a number between 1 and 7'
+// }
