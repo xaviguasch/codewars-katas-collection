f234517d59c1bdd37b8e37083a4778e59ca6b283
xaviguasch <xaviguasch@gmail.com>
2022-05-22 16:13:22 +0200

    added 8kyu 158-return-the-day kata

---
+++ 01-fundamentals--8-kyu/158-return-the-day.js
@@ -0,0 +1,20 @@
+// Complete the function which returns the weekday according to the input number:
+
+// 1 returns "Sunday"
+// 2 returns "Monday"
+// 3 returns "Tuesday"
+// 4 returns "Wednesday"
+// 5 returns "Thursday"
+// 6 returns "Friday"
+// 7 returns "Saturday"
+// Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+const whatDay = (num) => {
+  return
+}
+
+console.log(whatDay(1))
+console.log(whatDay(2))
+console.log(whatDay(3))
+console.log(whatDay(8))
+console.log(whatDay(20))
