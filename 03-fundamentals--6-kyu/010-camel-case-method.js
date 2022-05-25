7ec395fe757fbfba1253898fdb6345c3ecd063d9
xaviguasch <xaviguasch@gmail.com>
2022-05-25 19:56:41 +0200

    added alternative solutions to previous kata

--- 03-fundamentals--6-kyu/010-camel-case-method.js
+++ 03-fundamentals--6-kyu/010-camel-case-method.js
@@ -7,7 +7,7 @@
 
 String.prototype.camelCase = function () {
   return this.split(' ')
-    .map((word) => word.charAt().toUpperCase() + word.slice(1))
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join('')
 }
 
@@ -15,3 +15,32 @@ console.log('test case'.camelCase())
 console.log('camel case method'.camelCase())
 console.log('say hello'.camelCase())
 console.log(''.camelCase())
+
+// ALTERNATIVE SOLUTION
+
+// String.prototype.camelCase = function () {
+//   return this.split(' ')
+//     .map((w) => w.slice(0, 1).toUpperCase() + w.slice(1))
+//     .join('')
+// }
+
+// String.prototype.camelCase=function(){
+//   const capitalize = (word) => {
+//     return word.slice(0,1).toUpperCase() + word.slice(1)
+//   }
+
+//   return this
+//     .split(' ')
+//     .map(capitalize)
+//     .join('')
+// }
+
+// String.prototype.camelCase = function() {
+//   if (typeof this !== 'string' || !this.length) return '';
+
+//   return this
+//     .trim()
+//     .split(' ')
+//     .map(w => `${w[0].toUpperCase()}${w.toLowerCase().slice(1)}`)
+//     .join('');
+// }
