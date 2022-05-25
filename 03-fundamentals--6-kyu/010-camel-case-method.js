dce4c82106ed5bf5e4db9bd59b13ffefad4f96a6
xaviguasch <xaviguasch@gmail.com>
2022-05-25 19:43:01 +0200

    added 6kyu 010-camel-case-method kata

---
+++ 03-fundamentals--6-kyu/010-camel-case-method.js
@@ -0,0 +1,15 @@
+// Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+
+// For instance:
+
+// "hello case".camelCase() => HelloCase
+// "camel case word".camelCase() => CamelCaseWord
+
+String.prototype.camelCase = function () {
+  //your code here
+}
+
+console.log('test case'.camelCase())
+console.log('camel case method'.camelCase())
+console.log('say hello'.camelCase())
+console.log(''.camelCase())
