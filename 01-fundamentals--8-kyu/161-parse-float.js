ee88d9f339056ac60071851e9f8d157f517c7e6a
xaviguasch <xaviguasch@gmail.com>
2022-05-27 22:22:24 +0200

    added solution for previous kata

--- 01-fundamentals--8-kyu/161-parse-float.js
+++ 01-fundamentals--8-kyu/161-parse-float.js
@@ -1,7 +1,6 @@
 // Write function parseF which takes an input and returns a number or null if conversion is not possible. The input can be one of many different types so be aware.
 
-const parsef = (s) => {
-  return null
-}
+const parseF = (s) => (Number.isNaN(parseFloat(s)) ? null : parseFloat(s))
 
 console.log(parseF('1'))
+console.log(parseF('bbb'))
