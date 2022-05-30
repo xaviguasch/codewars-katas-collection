acef411824b2a2df53fdba40b0b2316c487f1943
xaviguasch <xaviguasch@gmail.com>
2022-05-30 22:58:29 +0200

    added alternative solutions to previous kata

--- 01-fundamentals--8-kyu/165-is-there-a-vowel-in-there.js
+++ 01-fundamentals--8-kyu/165-is-there-a-vowel-in-there.js
@@ -14,3 +14,10 @@ console.log(
 )
 
 console.log(isVow([101, 121, 110, 113, 113, 103, 121, 121, 101, 107, 103]))
+
+// ALTERNATIVE SOLUTIONS
+
+// const v = { 97: 'a', 101: 'e', 105: 'i', 111: 'o', 117: 'u' }
+// const isVow = (a) => a.map((x) => v[x] || x)
+
+// const isVow = a => a.map(x=>'aeiou'.includes(y=String.fromCharCode(x)) ? y : x)
