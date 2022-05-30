944336362ca390a5e62c97d880634d35c5fe2e67
xaviguasch <xaviguasch@gmail.com>
2022-05-30 21:19:36 +0200

    added solution for previous kata

--- 01-fundamentals--8-kyu/164-this-is-a-problem.js
+++ 01-fundamentals--8-kyu/164-this-is-a-problem.js
@@ -13,14 +13,25 @@
 // n.lastName //Expected: Doe
 // n.name //Expected: John Doe
 
-function NameMe(first, last) {
-  this.firstName = first
-  this.lastName = last
-  return { name: this.firstName + ' ' + this.lastName }
+// Initial code
+// function NameMe(first, last) {
+//   this.firstName = first
+//   this.lastName = last
+//   return { name: this.firstName + ' ' + this.lastName }
+// }
+
+// Solution code
+function NameMe(firstName, lastName) {
+  this.firstName = firstName
+  this.lastName = lastName
+  this.name = this.firstName + ' ' + this.lastName
 }
 
+//////////////////////////////////
 var n = new NameMe('John', 'Doe')
 
 console.log(n.firstName)
 console.log(n.lastName)
 console.log(n.name)
+
+console.log(n)
