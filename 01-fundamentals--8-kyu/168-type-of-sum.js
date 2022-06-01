75f7bc2719bc5c76076aefa5299a6e87b514f3b7
xaviguasch <xaviguasch@gmail.com>
2022-06-01 22:20:59 +0200

    added 8kyu 168-type-of-sum kata

---
+++ 01-fundamentals--8-kyu/168-type-of-sum.js
@@ -0,0 +1,11 @@
+// Return the type of the sum of the two arguments
+
+const typeOfSum = (a, b) => {
+  return
+}
+
+console.log(typeOfSum(12, 1))
+console.log(typeOfSum('d', 1))
+console.log(typeOfSum(1, 'a'))
+console.log(typeOfSum(true, 1))
+console.log(typeOfSum(null, undefined))
