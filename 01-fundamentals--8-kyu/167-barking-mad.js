9a12e58fe79fdec4bbf7cf2bce512dce774aa515
xaviguasch <xaviguasch@gmail.com>
2022-06-01 22:16:42 +0200

    added alternative solutions to previous kata

--- 01-fundamentals--8-kyu/167-barking-mad.js
+++ 01-fundamentals--8-kyu/167-barking-mad.js
@@ -16,5 +16,20 @@ var snoopy = new Dog('Beagle')
 
 var scoobydoo = new Dog('Great Dane')
 
-console.log(snoopy.bark())
-console.log(scoobydoo.bark())
+// ALTERNATIVE SOLUTION
+
+// class Dog {
+//   constructor(breed) {
+//       this.breed = breed;
+//   }
+
+//   bark() {
+//       return "Woof";
+//   }
+// }
+
+// let snoopy = new Dog("Beagle");
+// let scoobydoo = new Dog("Great Dane");
+
+// console.log(snoopy.bark())
+// console.log(scoobydoo.bark())
