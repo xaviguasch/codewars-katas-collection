3481fd567f11f0de34e984e27a14ee8ad5904ff0
xaviguasch <xaviguasch@gmail.com>
2022-06-02 22:44:47 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/169-price-of-mangoes.js
+++ 01-fundamentals--8-kyu/169-price-of-mangoes.js
@@ -4,9 +4,9 @@
 // mango(3, 3) ==> 6    # 2 mangoes for 3 = 6; +1 mango for free
 // mango(9, 5) ==> 30   # 6 mangoes for 5 = 30; +3 mangoes for free
 
-const mango = (quantity, price) => {
-  return
-}
+const mango = (quantity, price) =>
+  Math.floor(quantity / 3) * (price * 2) + (quantity % 3) * price
 
 console.log(mango(3, 3))
 console.log(mango(9, 5))
+console.log(mango(10, 5))
