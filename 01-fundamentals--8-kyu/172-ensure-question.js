45be35602045b3edf64e6c254c47fd0ab12b2399
xaviguasch <xaviguasch@gmail.com>
2022-06-05 23:02:23 +0200

    added 8kyu 172-ensure-question kata

---
+++ 01-fundamentals--8-kyu/172-ensure-question.js
@@ -0,0 +1,14 @@
+// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+// For example (Input --> Output)
+
+// "Yes" --> "Yes?"
+// "No?" --> "No?"
+
+const ensureQuestion = (s) => {
+  return
+}
+
+console.log(ensureQuestion(''))
+console.log(ensureQuestion('Yes'))
+console.log(ensureQuestion('No?'))
