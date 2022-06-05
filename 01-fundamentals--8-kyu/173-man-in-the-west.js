7ecc984878127397ec526028073b25b41534b466
xaviguasch <xaviguasch@gmail.com>
2022-06-05 23:16:23 +0200

    added 8kyu 173-man-in-the-west kata

---
+++ 01-fundamentals--8-kyu/173-man-in-the-west.js
@@ -0,0 +1,12 @@
+// A western man is trying to find gold in a river. To do that, he passes a bucket through the river's soil and then checks if it contains any gold. However, he could be more productive if he wrote an algorithm to do the job for him.
+
+// So, you need to check if there is gold in the bucket, and if so, return True/true. If not, return False/false.
+
+const checkTheBucket = (bucket) => {
+  return
+}
+
+console.log(checkTheBucket(['stone', 'stone', 'stone', 'stone', 'stone']))
+console.log(checkTheBucket(['stone', 'stone', 'stone', 'stone', 'gold']))
+console.log(checkTheBucket(['gold', 'stone', 'stone', 'stone', 'stone']))
+console.log(checkTheBucket([]))
