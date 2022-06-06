adbe8806b71013b49ae52ac06d1d307f55dfa621
xaviguasch <xaviguasch@gmail.com>
2022-06-06 09:33:09 +0200

    added alternative solutions

--- 02-fundamentals--7-kyu/076-number-of-occurrences.js
+++ 02-fundamentals--7-kyu/076-number-of-occurrences.js
@@ -20,3 +20,22 @@ console.log(arr.numberOfOccurrences(0))
 console.log(arr.numberOfOccurrences(4))
 console.log(arr.numberOfOccurrences(2))
 console.log(arr.numberOfOccurrences('a'))
+
+// ALERNATIVE SOLUTIONS
+
+// Array.prototype.numberOfOccurrences = function (element) {
+
+//   var counter = 0;
+//   for (var i = 0; i < this.length; i++)
+//   {
+//       if (this[i] == element) { counter++; }
+//   }
+
+//   return counter;
+// }
+
+// Array.prototype.numberOfOccurrences = function(element) {
+//   return this.reduce(function(acc, current) {
+//     return current === element ? acc + 1 : acc;
+//   }, 0);
+// }
