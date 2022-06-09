6b48809b239543c4eb41d5121ccf460cf8fcb9de
xaviguasch <xaviguasch@gmail.com>
2022-06-09 22:37:47 +0200

    added 7kyu 079-switcheroo kata

---
+++ 02-fundamentals--7-kyu/079-switcheroo.js
@@ -0,0 +1,14 @@
+// Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+// Example:
+
+// 'acb' --> 'bca'
+// 'aabacbaa' --> 'bbabcabb'
+
+const switcheroo = (x) => {
+  return
+}
+
+console.log(switcheroo('abc'))
+console.log(switcheroo('aaabcccbaaa'))
+console.log(switcheroo('ccccc'))
