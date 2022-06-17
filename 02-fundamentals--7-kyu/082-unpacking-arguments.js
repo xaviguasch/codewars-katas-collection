63194af5832ebc882941978d410cba4628f3d4a5
xaviguasch <xaviguasch@gmail.com>
2022-06-17 21:10:16 +0200

    added 7kyu 082-upacking-arguments kata

---
+++ 02-fundamentals--7-kyu/082-unpacking-arguments.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// You must create a function, spread, that takes a function and a list of arguments to be applied to that function. You must make this function return the result of calling the given function/lambda with the given arguments.
+
+// eg:
+
+// spread(someFunction, [1, true, "Foo", "bar"] )
+// is the same as...
+// someFunction(1, true, "Foo", "bar")
+
+const spread = (func, args) => {
+  return
+}
+
+console.log(spread(someFunction, [1, true, 'Foo', 'bar']))
