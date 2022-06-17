1a4da3056a2f83bf4687a49a3eb39fc88509974c
xaviguasch <xaviguasch@gmail.com>
2022-06-17 21:13:31 +0200

    solved previous kata

--- 02-fundamentals--7-kyu/082-unpacking-arguments.js
+++ 02-fundamentals--7-kyu/082-unpacking-arguments.js
@@ -7,8 +7,12 @@
 // is the same as...
 // someFunction(1, true, "Foo", "bar")
 
+const someFunction = (a, b, c, d) => {
+  console.log(a, b, c, d)
+}
+
 const spread = (func, args) => {
-  return
+  return func(...args)
 }
 
 console.log(spread(someFunction, [1, true, 'Foo', 'bar']))
