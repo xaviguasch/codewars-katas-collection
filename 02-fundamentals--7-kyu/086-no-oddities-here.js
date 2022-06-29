f61740cad6d6cf3378948f6dc53184354e6f234a
xaviguasch <xaviguasch@gmail.com>
2022-06-29 23:16:09 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/086-no-oddities-here.js
+++ 02-fundamentals--7-kyu/086-no-oddities-here.js
@@ -2,9 +2,7 @@
 
 // All values in the array will be integers. Return the good values in the order they are given.
 
-const noOdds = (values) => {
-  return
-}
+const noOdds = (values) => values.filter((val) => val % 2 === 0)
 
 console.log(noOdds([0, 1]))
 console.log(noOdds([0, 1, 2, 3]))
