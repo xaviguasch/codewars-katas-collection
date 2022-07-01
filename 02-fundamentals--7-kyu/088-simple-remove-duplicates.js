77590a564ed37d1dff0923409e1cf0cb0487ea6f
xaviguasch <xaviguasch@gmail.com>
2022-07-01 22:27:02 +0200

    added 7kyu 088-simple-remove-duplicates kata

---
+++ 02-fundamentals--7-kyu/088-simple-remove-duplicates.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+// Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+// Example:
+// For input: [3, 4, 4, 3, 6, 3]
+
+// remove the 3 at index 0
+// remove the 4 at index 1
+// remove the 3 at index 3
+// Expected output: [4, 6, 3]
+
+// More examples can be found in the test cases.
+
+// Good luck!
+
+const solve = (arr) => {
+  return
+}
+
+console.log(solve([3, 4, 4, 3, 6, 3]))
+console.log(solve([1, 2, 1, 2, 1, 2, 3]))
+console.log(solve([1, 2, 3, 4]))
+console.log(solve([1, 1, 4, 5, 1, 2, 1]))
+console.log(solve([1, 2, 1, 2, 1, 1, 3]))
