864372e4c017400391f97340497d28b7fdcb52a0
xaviguasch <xaviguasch@gmail.com>
2022-07-14 23:43:46 +0200

    added 7kyu 089-ordered-count-of-characters kata

---
+++ 02-fundamentals--7-kyu/089-ordered-count-of-characters.js
@@ -0,0 +1,13 @@
+// Count the number of occurrences of each character and return it as a list of tuples in order of appearance. For empty output return an empty list.
+
+// Example:
+
+// orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
+
+const orderedCount = (text) => {
+  return []
+}
+
+console.log(orderedCount('abracadabra'))
+console.log(orderedCount('Code Wars'))
+console.log(orderedCount('233312'))
