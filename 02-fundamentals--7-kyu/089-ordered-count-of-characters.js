4497953521a545bba3621dd6f47a1950d4bda359
xaviguasch <xaviguasch@gmail.com>
2022-07-14 23:58:59 +0200

    added alternative solutions

--- 02-fundamentals--7-kyu/089-ordered-count-of-characters.js
+++ 02-fundamentals--7-kyu/089-ordered-count-of-characters.js
@@ -15,3 +15,46 @@ const orderedCount = (text) => {
 console.log(orderedCount('abracadabra'))
 console.log(orderedCount('Code Wars'))
 console.log(orderedCount('233312'))
+
+// ALTERNATIVE SOLUTIONS
+
+// const orderedCount = (s) =>
+//   Array.from(
+//     s.split('').reduce((m, k) => m.set(k, m.has(k) ? m.get(k) + 1 : 1), new Map())
+//   )
+
+// const orderedCount = (str) =>
+//   [...new Set([...str])].map((char) => [char, str.split(char).length - 1])
+
+// var orderedCount = function (text) {
+//   var arr = []
+//   for (var i = 0; i < text.length; ++i) {
+//     var k = 0
+//     for (var j = 0; j < arr.length; ++j) if (arr[j][0] == text[i]) k++
+//     if (k == 0) {
+//       for (var j = i; j < text.length; ++j) if (text[j] == text[i]) k++
+//       arr.push([text[i], k])
+//     }
+//   }
+//   return arr
+// }
+
+// const orderedCount = (text) => {
+//   // O(n)
+//   const result = []
+//   const letters = {}
+
+//   for (let i = 0, l = text.length; i < l; i++) {
+//     const char = text[i]
+//     const index = letters[char]
+
+//     if (index === undefined) {
+//       letters[char] = i
+//       result.push([char, 1])
+//     } else {
+//       result[index][1]++
+//     }
+//   }
+
+//   return result
+// }
