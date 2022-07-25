028f9112c23e933b0cb959fb8b6a3c15bd7eca6d
xaviguasch <xaviguasch@gmail.com>
2022-07-25 16:04:09 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/090-filter-the-number.js
+++ 02-fundamentals--7-kyu/090-filter-the-number.js
@@ -8,7 +8,13 @@
 // You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
 
 const FilterString = (value) => {
-  return
+  const string = value
+    .split('')
+    .map((char) => Number(char))
+    .filter((el) => !isNaN(el))
+    .join('')
+
+  return Number(string)
 }
 
 console.log(FilterString('123'))
