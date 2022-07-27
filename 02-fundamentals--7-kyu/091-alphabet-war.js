43144caf2f088adb8e06ea22caf18fca8c6047da
xaviguasch <xaviguasch@gmail.com>
2022-07-27 21:09:06 +0200

    added alternative solutions

--- 02-fundamentals--7-kyu/091-alphabet-war.js
+++ 02-fundamentals--7-kyu/091-alphabet-war.js
@@ -51,3 +51,11 @@ console.log(alphabetWar('z'))
 console.log(alphabetWar('zdqmwpbs'))
 console.log(alphabetWar('zzzzs'))
 console.log(alphabetWar('wwwwww'))
+
+// ALTERNATIVE SOLUTIONS
+
+// function alphabetWar(fight) {
+//   let map = { w: -4, p: -3, b: -2, s: -1, m: 4, q: 3, d: 2, z: 1 }
+//   let result = fight.split('').reduce((a, b) => a + (map[b] || 0), 0)
+//   return result ? (result < 0 ? 'Left' : 'Right') + ' side wins!' : "Let's fight again!"
+// }
