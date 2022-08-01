167cf98e412fc665213164c9cca5f432e42a0cfa
xaviguasch <xaviguasch@gmail.com>
2022-08-01 23:12:24 +0200

    added 7kyu 093-reverse-a-number kata

---
+++ 02-fundamentals--7-kyu/093-reverse-a-number.js
@@ -0,0 +1,19 @@
+// DESCRIPTION:
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+// Examples
+//  123 ->  321
+// -456 -> -654
+// 1000 ->    1
+
+const reverseNumber = (n) => {}
+
+console.log(reverseNumber(123))
+console.log(reverseNumber(-123))
+console.log(reverseNumber(1000))
+console.log(reverseNumber(4321234))
+console.log(reverseNumber(5))
+console.log(reverseNumber(0))
+console.log(reverseNumber(98989898))
