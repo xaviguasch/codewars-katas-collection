500b69e891d91778922449668f629874bf8e79e9
xaviguasch <xaviguasch@gmail.com>
2022-08-01 23:07:59 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/092-add-property-to-every-object-in-array.js
+++ 02-fundamentals--7-kyu/092-add-property-to-every-object-in-array.js
@@ -25,3 +25,22 @@
 //     usersAnswer: null
 // }];
 // The questions array is already defined for you and is not the same as the one in the example.
+
+var questions = [
+  {
+    question: "What's the currency of the USA?",
+    choices: ['US dollar', 'Ruble', 'Horses', 'Gold'],
+    corAnswer: 0,
+  },
+  {
+    question: 'Where was the American Declaration of Independence signed?',
+    choices: ['Philadelphia', 'At the bottom', "Frankie's Pub", 'China'],
+    corAnswer: 0,
+  },
+]
+
+questions.forEach((q) => {
+  q.userAnswer = null
+})
+
+console.log(questions)
