f0a7f58fcae2859fdc20f5a72b6b4a7bdb239e6b
xaviguasch <xaviguasch@gmail.com>
2022-08-05 21:12:08 +0200

    added 7kyu 095-substituting-variables-into-strings-padded-numbers kata

---
+++ 02-fundamentals--7-kyu/095-substituting-variables-into-strings-padded-numbers.js
@@ -0,0 +1,14 @@
+// Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+// Example:
+
+// solution(5) // should return "Value is 00005"
+
+const solution = (value) => {
+  return
+}
+
+console.log(solution(5))
+console.log(solution(1204))
+console.log(solution(109))
+console.log(solution(0))
