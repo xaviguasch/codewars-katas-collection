f9b436ac8ea7d8eff43e8c61f4fc55bb19bf3b90
xaviguasch <xaviguasch@gmail.com>
2022-08-17 22:38:22 +0200

    added 7kyu 099-anagram-detection kata

---
+++ 02-fundamentals--7-kyu/099-anagram-detection.js
@@ -0,0 +1,21 @@
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+// Examples
+// "foefet" is an anagram of "toffee"
+
+// "Buckethead" is an anagram of "DeathCubeK"
+
+const isAnagram = (test, original) => {
+  return
+}
+
+console.log(isAnagram('foefet', 'toffee'))
+console.log(isAnagram('Buckethead', 'DeathCubeK'))
+console.log(isAnagram('Twoo', 'WooT'))
+console.log(isAnagram('dumble', 'bumble'))
+console.log(isAnagram('ound', 'round'))
+console.log(isAnagram('apple', 'pale'))
