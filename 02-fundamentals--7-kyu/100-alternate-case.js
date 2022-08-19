48df1fd1f3a04f60d37e8ade6f4195ee4c2ecf5b
xaviguasch <xaviguasch@gmail.com>
2022-08-19 21:30:04 +0200

    added 7kyu 100-alternate-case kata

---
+++ 02-fundamentals--7-kyu/100-alternate-case.js
@@ -0,0 +1,9 @@
+// Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+
+const alternateCase = (s) => {
+  return
+}
+
+console.log(alternateCase('abc'))
+console.log(alternateCase('ABC'))
+console.log(alternateCase('Hello World'))
