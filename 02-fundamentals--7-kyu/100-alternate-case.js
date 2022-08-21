851aa29a72893d5c95c73f049e4b8488a89b72a6
xaviguasch <xaviguasch@gmail.com>
2022-08-21 22:12:58 +0200

    added solution to previous kata

--- 02-fundamentals--7-kyu/100-alternate-case.js
+++ 02-fundamentals--7-kyu/100-alternate-case.js
@@ -1,7 +1,12 @@
 // Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
 
 const alternateCase = (s) => {
-  return
+  return s
+    .split('')
+    .map((letter) =>
+      letter.toLowerCase() === letter ? letter.toUpperCase() : letter.toLowerCase()
+    )
+    .join('')
 }
 
 console.log(alternateCase('abc'))
