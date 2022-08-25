53e800a37544c9de56b72c02ab738a05f21d1443
xaviguasch <xaviguasch@gmail.com>
2022-08-25 15:02:03 +0200

    added strings 8kyu 176-a-strange-trip-to-the-market kata

--- 01-fundamentals--8-kyu/175-holiday-VI-shark-pontoon.js
+++ 01-fundamentals--8-kyu/175-holiday-VI-shark-pontoon.js
@@ -31,3 +31,14 @@ const shark = (pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) =>
 console.log(shark(12, 50, 4, 8, true))
 console.log(shark(7, 55, 4, 16, true))
 console.log(shark(24, 0, 4, 8, true))
+
+// ADDED ALTERNATIVE SOLUTIONS
+
+// function shark(pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) {
+//   if (dolphin) {
+//     sharkSpeed /= 2
+//   }
+//   return pontoonDistance / youSpeed < sharkDistance / sharkSpeed
+//     ? 'Alive!'
+//     : 'Shark Bait!'
+// }
