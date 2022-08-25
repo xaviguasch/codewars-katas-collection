715139176b914ae1e382d9426289fa5374e34b7e
xaviguasch <xaviguasch@gmail.com>
2022-08-25 15:03:45 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/176-a-strange-trip-to-the-market.js
+++ 01-fundamentals--8-kyu/176-a-strange-trip-to-the-market.js
@@ -8,7 +8,11 @@
 // Since Nessie is a master of disguise, the only way accurately tell is to look for the phrase "tree fiddy". Since you are tired of being grifted by this monster, the time has come to code a solution for finding The Loch Ness Monster. Note that the phrase can also be written as "3.50" or "three fifty".
 
 const isLockNessMonster = (s) => {
-  return
+  if (s.includes('tree fiddy') || s.includes('3.50') || s.includes('three fifty')) {
+    return true
+  } else {
+    return false
+  }
 }
 
 console.log(
