28361787cf7be6ab83523f572a54e9f97da1a1dc
xaviguasch <xaviguasch@gmail.com>
2022-08-26 19:20:21 +0200

    added solution to previous kata

--- 01-fundamentals--8-kyu/178-arrow-style-functions.js
+++ 01-fundamentals--8-kyu/178-arrow-style-functions.js
@@ -8,9 +8,7 @@
 // Javascript => String.fromCharCode(97) // a
 // C# => Tools.FromCharCode(97) // a
 
-// var ArrowFunc = function(arr) {
-//   return arr.map( => ).join(''); //Complete this function
-// }
+const ArrowFunc = (arr) => arr.map((num) => String.fromCharCode(num)).join('')
 
 console.log(ArrowFunc([84, 101, 115, 116]))
 console.log(ArrowFunc([70, 85, 83, 32, 82, 79, 72, 32, 68, 65, 72]))
