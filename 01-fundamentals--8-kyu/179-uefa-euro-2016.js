c76d5374aa4958d61c4b4825d473c2dbc4d5a856
xaviguasch <xaviguasch@gmail.com>
2022-08-27 21:03:50 +0200

    added alternative solution to previous kata

--- 01-fundamentals--8-kyu/179-uefa-euro-2016.js
+++ 01-fundamentals--8-kyu/179-uefa-euro-2016.js
@@ -19,3 +19,15 @@ const uefaEuro2016 = (teams, scores) => {
 console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0]))
 console.log(uefaEuro2016(['Belgium', 'Italy'], [0, 2]))
 console.log(uefaEuro2016(['Portugal', 'Iceland'], [1, 1]))
+
+// ALTERNATIVE SOLUTION
+
+// function uefaEuro2016(teams, scores) {
+//   const [team1, team2] = teams
+//   const [score1, score2] = scores
+//   const winner = score1 > score2 ? team1 : team2
+
+//   return score1 === score2
+//     ? `At match ${team1} - ${team2}, teams played draw.`
+//     : `At match ${team1} - ${team2}, ${winner} won!`
+// }
