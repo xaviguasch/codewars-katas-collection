8446d633e95ef858b2ab19d4218996d7092340c9
xaviguasch <xaviguasch@gmail.com>
2022-08-27 21:08:06 +0200

    added strings 8kyu 180-get-number-string kata

---
+++ 01-fundamentals--8-kyu/180-get-number-from-string.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+// Function:
+
+// getNumberFromString(s)
+
+const getNumberFromString = (s) => {
+  return
+}
+
+console.log(getNumberFromString('1'))
+console.log(getNumberFromString('123'))
+console.log(getNumberFromString('this is number: 7'))
