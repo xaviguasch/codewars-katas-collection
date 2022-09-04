ec468f135e78f40c07900a4e9a7ba46517e5bdf3
xaviguasch <xaviguasch@gmail.com>
2022-09-04 12:01:22 +0200

    added strings 7kyu 105-digits-explosion kata

---
+++ 02-fundamentals--7-kyu/105-digits-explosion.js
@@ -0,0 +1,21 @@
+// Given a string made of digits [0-9], return a string where each digit is repeated a number of times equals to its value.
+
+// Examples
+// explode("312")
+// should return :
+
+// "333122"
+// explode("102269")
+// should return :
+
+// "12222666666999999999"
+
+const explode = (s) => {
+  return
+}
+
+console.log(explode('312'))
+console.log(explode('102269'))
+console.log(explode('0'))
+console.log(explode('000'))
+console.log(explode('123'))
