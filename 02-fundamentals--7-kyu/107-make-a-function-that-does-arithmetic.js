334552e06004e3f5e8063200a9eb742d380e520a
xaviguasch <xaviguasch@gmail.com>
2022-09-05 17:06:52 +0200

    added alternative solution to previous kata

--- 02-fundamentals--7-kyu/107-make-a-function-that-does-arithmetic.js
+++ 02-fundamentals--7-kyu/107-make-a-function-that-does-arithmetic.js
@@ -24,3 +24,18 @@ console.log(arithmetic(1, 2, 'add'))
 console.log(arithmetic(8, 2, 'subtract'))
 console.log(arithmetic(5, 2, 'multiply'))
 console.log(arithmetic(8, 2, 'divide'))
+
+// ALTERNATIVE SOLUTION
+
+function arithmetic(a, b, operator) {
+  switch (operator) {
+    case 'add':
+      return a + b
+    case 'subtract':
+      return a - b
+    case 'multiply':
+      return a * b
+    case 'divide':
+      return a / b
+  }
+}
