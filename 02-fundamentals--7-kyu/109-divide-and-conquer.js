b78cc6944f2f910ec73a3e20c0ba3de8c9924254
xaviguasch <xaviguasch@gmail.com>
2022-09-06 20:11:19 +0200

    added strings 7kyu 109-divide-and-conquer kata

---
+++ 02-fundamentals--7-kyu/109-divide-and-conquer.js
@@ -0,0 +1,11 @@
+// Given a mixed array of number and string representations of integers, add up the string integers and subtract this from the total of the non-string integers.
+
+// Return as a number.
+
+const divCon = (x) => {
+  return
+}
+
+console.log(divCon([9, 3, '7', '3']))
+console.log(divCon(['5', '0', 9, 3, 2, 1, '9', 6, 7]))
+console.log(divCon(['3', 6, 6, 0, '5', 8, 5, '6', 2, '0']))
