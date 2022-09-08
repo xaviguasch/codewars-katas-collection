3d9eca9160cc984761a7dc2ab994bcbf903157d2
xaviguasch <xaviguasch@gmail.com>
2022-09-08 19:46:21 +0200

    added arrays 8kyu 189-sum-of-differences-in-array kata

---
+++ 01-fundamentals--8-kyu/189-sum-of-differences-in-arrays.js
@@ -0,0 +1,16 @@
+// Your task is to sum the differences between consecutive pairs in the array in descending order.
+
+// Example
+// [2, 1, 10]  -->  9
+// In descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
+
+const sumOfDifferences = (arr) => {
+  return
+}
+
+console.log(sumOfDifferences([1, 2, 10]))
+console.log(sumOfDifferences([-3, -2, -1]))
