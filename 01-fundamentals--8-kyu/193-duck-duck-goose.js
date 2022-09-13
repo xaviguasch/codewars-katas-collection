36c539aab04cf398c95817b3f3cdfc8387cc7c8d
xaviguasch <xaviguasch@gmail.com>
2022-09-13 15:44:38 +0200

    added arrays 8kyu 193-duck-duck-goose kata

---
+++ 01-fundamentals--8-kyu/193-duck-duck-goose.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+// The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
+
+// Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+
+// Example:
+
+// duck_duck_goose([a, b, c, d], 1) should return a.name
+// duck_duck_goose([a, b, c, d], 5) should return a.name
+// duck_duck_goose([a, b, c, d], 4) should return d.name
+
+const duckDuckGoose = (players, goose) => {
+  return
+}
+
+console.log(duckDuckGoose(players, 1))
+console.log(duckDuckGoose(players, 3))
+console.log(duckDuckGoose(players, 10))
+console.log(duckDuckGoose(players, 20))
+console.log(duckDuckGoose(players, 30))
+console.log(duckDuckGoose(players, 18))
+console.log(duckDuckGoose(players, 12))
+console.log(duckDuckGoose(players, 2))
+console.log(duckDuckGoose(players, 7))
