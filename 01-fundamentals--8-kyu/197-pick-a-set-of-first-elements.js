5fadea43e6a47731d6be32c39bb1eda6a7ca50bc
xaviguasch <xaviguasch@gmail.com>
2022-09-15 23:10:30 +0200

    added arrays 8kyu 197-pick-a-set-of-first-elements kata

---
+++ 01-fundamentals--8-kyu/197-pick-a-set-of-first-elements.js
@@ -0,0 +1,21 @@
+// Write a function to get the first element(s) of a sequence. Passing a parameter n (default=1) will return the first n element(s) of the sequence.
+
+// If n == 0 return an empty sequence []
+
+// Examples
+// var arr = ['a', 'b', 'c', 'd', 'e'];
+// first(arr) //=> ['a'];
+// first(arr, 2) //=> ['a', 'b']
+// first(arr, 3) //=> ['a', 'b', 'c'];
+// first(arr, 0) //=> [];
+
+const first = (arr, n) => {
+  return
+}
+
+var arr = ['a', 'b', 'c', 'd', 'e']
+
+console.log(arr)
+console.log(arr, 2)
+console.log(arr, 3)
+console.log(arr, 0)
