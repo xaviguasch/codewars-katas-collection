1d011089ff0fb945ca962b386625f3b6eaf0fe0f
xaviguasch <xaviguasch@gmail.com>
2022-09-16 19:45:34 +0200

    added arrays 8kyu 198-enumerable-magic-20-cascading-subsets kata

---
+++ 01-fundamentals--8-kyu/198-enumerable-magic-20-cascading-subsets.js
@@ -0,0 +1,21 @@
+// DESCRIPTION:
+// Create a method each_cons that accepts a list and a number n, and returns cascading subsets of the list of size n, like so:
+
+// each_cons([1,2,3,4], 2)
+//   #=> [[1,2], [2,3], [3,4]]
+
+// each_cons([1,2,3,4], 3)
+//   #=> [[1,2,3],[2,3,4]]
+
+// As you can see, the lists are cascading; ie, they overlap, but never out of order.
+
+const eachCons = (array, n) => {
+  return
+}
+
+const lst = [3, 5, 8, 13]
+
+console.log(eachCons(lst, 1))
+console.log(eachCons(lst, 2))
+console.log(eachCons(lst, 3))
+console.log(eachCons([], 3))
