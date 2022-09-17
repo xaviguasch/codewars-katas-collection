47224217e417441bb8d0b7a8a15c44a0e9891635
xaviguasch <xaviguasch@gmail.com>
2022-09-17 18:57:51 +0200

    solved previous kata

--- 02-fundamentals--7-kyu/110-fizz-buzz.js
+++ 02-fundamentals--7-kyu/110-fizz-buzz.js
@@ -12,7 +12,21 @@
 // fizzbuzz(3) -->  [1, 2, "Fizz"]
 
 const fizzbuzz = (n) => {
-  return
+  const resultArr = []
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      resultArr.push('FizzBuzz')
+    } else if (i % 3 === 0) {
+      resultArr.push('Fizz')
+    } else if (i % 5 === 0) {
+      resultArr.push('Buzz')
+    } else {
+      resultArr.push(i)
+    }
+  }
+
+  return resultArr
 }
 
 console.log(fizzbuzz(3))
