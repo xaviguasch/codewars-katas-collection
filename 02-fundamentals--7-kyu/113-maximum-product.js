7b798a1653939550ea92ae3a1fe2b6c624fb1de3
xaviguasch <xaviguasch@gmail.com>
2022-09-19 11:40:13 +0200

    added arrays 113-maximum-product kata

---
+++ 02-fundamentals--7-kyu/113-maximum-product.js
@@ -0,0 +1,32 @@
+// DESCRIPTION:
+// Task
+// Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+// Notes
+// Array/list size is at least 2.
+
+// Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+// Input >> Output Examples
+// adjacentElementsProduct([1, 2, 3]); ==> return 6
+// Explanation:
+// The maximum product obtained from multiplying 2 * 3 = 6, and they're adjacent numbers in the array.
+// adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48]); ==> return 50
+// Explanation:
+// Max product obtained from multiplying 5 * 10  =  50 .
+
+// adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921])  ==>  return -14
+// Explanation:
+// The maximum product obtained from multiplying -2 * 7 = -14, and they're adjacent numbers in the array.
+
+const adjacentElementsProduct = (array) => {
+  return
+}
+
+console.log(adjacentElementsProduct([5, 8]))
+console.log(adjacentElementsProduct([1, 2, 3]))
+console.log(adjacentElementsProduct([1, 5, 10, 9]))
+console.log(adjacentElementsProduct([5, 1, 2, 3, 1, 4]))
+
+console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]))
+console.log(adjacentElementsProduct([9, 5, 10, 2, 24, -1, -48]))
