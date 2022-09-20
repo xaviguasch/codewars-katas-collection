976a0656f482c386c0702c2013b5652bc1c1f719
xaviguasch <xaviguasch@gmail.com>
2022-09-20 11:25:26 +0200

    added arrays 116-spacify kata

---
+++ 02-fundamentals--7-kyu/116-spacify.js
@@ -0,0 +1,10 @@
+// Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+// spacify("hello world") // returns "h e l l o   w o r l d"
+
+const spacify = (str) => {
+  return
+}
+
+console.log(spacify('hello world'))
+console.log(spacify('12345'))
