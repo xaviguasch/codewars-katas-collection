eed49965690ad29cab0d2251b03034cf2d165f32
xaviguasch <xaviguasch@gmail.com>
2022-09-21 22:05:49 +0200

    added arrays 7kyu 117-largest-elements kata

---
+++ 02-fundamentals--7-kyu/117-largest-elements.js
@@ -0,0 +1,15 @@
+// Write a program that outputs the top n elements from a list.
+
+// Example:
+
+// largest(2, [7,6,5,4,3,2,1])
+// => [6,7]
+
+const largest = (n, xs) => {
+  return
+}
+
+console.log(largest(2, [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]))
+console.log(largest(3, [5, 1, 5, 2, 3, 1, 2, 3, 5]))
+console.log(largest(7, [9, 1, 50, 22, 3, 13, 2, 63, 5]))
+console.log(largest(0, [1, 2, 3, 4, 8, 7, 6, 5]))
