a4d74aab9858931ddce14dae643476bcc77bd3a9
xaviguasch <xaviguasch@gmail.com>
2022-10-07 21:02:13 +0200

    added strings 7kyu 119-all-unique kata

---
+++ 02-fundamentals--7-kyu/119-all-unique.js
@@ -0,0 +1,12 @@
+// Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
+
+// The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.
+
+const hasUniqueChars = (str) => {
+  return
+}
+
+console.log(hasUniqueChars('  nAa'))
+console.log(hasUniqueChars('abcdef'))
+console.log(hasUniqueChars('aA'))
+console.log(hasUniqueChars('++-'))
