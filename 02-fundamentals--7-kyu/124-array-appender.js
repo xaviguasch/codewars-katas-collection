874030c1b0eb9bbe0bd146a22c3fa14d5652fd03
xaviguasch <xaviguasch@gmail.com>
2022-11-03 20:42:25 +0100

    added arrays 124-array-appender kata

---
+++ 02-fundamentals--7-kyu/124-array-appender.js
@@ -0,0 +1,18 @@
+// Write a function that appends the items from sequence 2 onto sequence 1, returning the newly formed sequence. Your function should also be able to handle nested sequences.
+
+// All inputs will be arrays/nested arrays.
+
+// For example:
+
+// ['a','b','c'], [1,2,3]     --> ['a','b','c',1,2,3]
+// [['x','x'],'B'], ['c','D'] --> [['x','x'],'B','c','D']
+
+const appendArrays = (arr1, arr2) => {
+  return
+}
+
+console.log(appendArrays([1, 2], [2, 4]))
+console.log(appendArrays([1, 2], [3, 4]))
+console.log(appendArrays(['this'], ['that']))
+console.log(appendArrays(['a', 'B'], ['c', 'D']))
+console.log(appendArrays([1, 2], [1]))
