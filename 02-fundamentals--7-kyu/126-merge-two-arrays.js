fdb2f88d4e3009e67ed2aa35b0ef59110bf45f68
xaviguasch <xaviguasch@gmail.com>
2022-11-19 20:01:24 +0100

    added arrays 126-merge-two-arrays kata

---
+++ 02-fundamentals--7-kyu/126-merge-two-arrays.js
@@ -0,0 +1,22 @@
+// Write a function that combines two arrays by alternatingly taking elements from each array in turn.
+
+// Examples:
+
+// [a, b, c, d, e], [1, 2, 3, 4, 5] becomes  [a, 1, b, 2, c, 3, d, 4, e, 5]
+
+// [1, 2, 3], [a, b, c, d, e, f] becomes [1, a, 2, b, 3, c, d, e, f]
+// Points:
+
+// The arrays may be of different lengths, with at least one character/digit.
+// One array will be of string characters (in lower case, a-z), a second of integers (all positive starting at 1).
+
+const mergeArrays = (a, b) => {
+  return
+}
+
+console.log(mergeArrays([1, 2, 3, 4, 5, 6, 7, 8], ['a', 'b', 'c', 'd', 'e']))
+console.log(mergeArrays(['a', 'b', 'c', 'd', 'e'], [1, 2, 3, 4, 5]))
+console.log(mergeArrays([2, 5, 8, 23, 67, 6], ['b', 'r', 'a', 'u', 'r', 's']))
+console.log(
+  mergeArrays(['b', 'r', 'a', 'u', 'r', 's', 'e', 'q', 'z'], [2, 5, 8, 23, 67, 6])
+)
