2bf9fe08a4e73edf3cd54e7bdc297f31f27faaf4
xaviguasch <xaviguasch@gmail.com>
2022-11-24 21:43:03 +0100

    added arrays 129-sum-all-the-arrays kata

---
+++ 02-fundamentals--7-kyu/129-sum-all-the-arrays.js
@@ -0,0 +1,13 @@
+// You are given an array of values.
+
+// Sum every number value in the array, and any nested arrays (to any depth).
+
+// Ignore all other types of values.
+
+const arraySum = (arr) => {
+  return
+}
+
+console.log(arraySum([1, 2]))
+console.log(arraySum([1, 2, 3]))
+console.log(arraySum([1, 2, [1, 2]]))
