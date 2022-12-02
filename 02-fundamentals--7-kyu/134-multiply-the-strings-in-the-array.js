6be4ad414d8c3383988af8e04ca78b6c2cf79c6e
xaviguasch <xaviguasch@gmail.com>
2022-12-02 20:47:20 +0100

    added arrays 134-multiply-the-strings-in-the-array kata

---
+++ 02-fundamentals--7-kyu/134-multiply-the-strings-in-the-array.js
@@ -0,0 +1,11 @@
+// You received an array with two strings. Create a function that will return their product as a string. E.g.
+
+// arrMultiply(['9','6']) should return '54'
+
+const arrMultiply = (arr) => {
+  return
+}
+
+console.log(arrMultiply(['4', '5']))
+console.log(arrMultiply(['2', '-5']))
+console.log(arrMultiply(['9', '0']))
