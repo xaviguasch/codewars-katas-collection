97b7ff0b4b3eadd3b04b44491385ae95fa8a86f1
xaviguasch <xaviguasch@gmail.com>
2022-12-04 17:40:47 +0100

    added solution to 131-ultimate-array-reverser kata

--- 02-fundamentals--7-kyu/131-ultimate-array-reverser.js
+++ 02-fundamentals--7-kyu/131-ultimate-array-reverser.js
@@ -1,11 +1,19 @@
 // Given an array of strings, reverse them and their order in such way that their length stays the same as the length of the original inputs.
 
 // Example:
-// Input:  {"I", "like", "big", "butts", "and", "I", "cannot", "lie!"}
-// Output: {"!", "eilt", "onn", "acIdn", "ast", "t", "ubgibe", "kilI"}
+// Input:  ["I", "like", "big", "butts", "and", "I", "cannot", "lie!"]
+// Output: ["!", "eilt", "onn", "acIdn", "ast", "t", "ubgibe", "kilI"]
 
 const ultimateReverse = (s) => {
-  return
+  const reversedArr = s.join('').split('').reverse()
+
+  const finalArr = []
+
+  for (let i = 0; i < s.length; i++) {
+    finalArr.push(reversedArr.splice(0, s[i].length))
+  }
+
+  return finalArr.map((innerArr) => innerArr.join(''))
 }
 
 console.log(ultimateReverse(['I', 'like', 'big', 'butts', 'and', 'I', 'cannot', 'lie!']))
