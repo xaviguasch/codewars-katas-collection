2cc2e743950d14dbe6021c483821e5f664cd9e42
xaviguasch <xaviguasch@gmail.com>
2022-12-07 18:50:38 +0100

    added alternative solutions to kata n132

--- 02-fundamentals--7-kyu/132-array-element-parity.js
+++ 02-fundamentals--7-kyu/132-array-element-parity.js
@@ -23,3 +23,9 @@ console.log(solve([-3, 1, 2, 3, -1, -4, -2]))
 console.log(solve([1, -1, 2, -2, 3, 3]))
 console.log(solve([-110, 110, -38, -38, -62, 62, -38, -38, -38]))
 console.log(solve([-9, -105, -9, -9, -9, -9, 105]))
+
+// ALTERNATIVE SOLUTIONS
+
+// const solve = array => array.find(value => !array.includes(-value))
+
+// const solve = arr => [...new Set(arr)].reduce((a, c) => a + c);
