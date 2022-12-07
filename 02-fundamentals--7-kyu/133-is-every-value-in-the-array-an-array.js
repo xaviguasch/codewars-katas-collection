1f42825bb0c6e61c29cd2796a2122975bbd0eb12
xaviguasch <xaviguasch@gmail.com>
2022-12-07 18:59:57 +0100

    added alternative solution to previous kata

--- 02-fundamentals--7-kyu/133-is-every-value-in-the-array-an-array.js
+++ 02-fundamentals--7-kyu/133-is-every-value-in-the-array-an-array.js
@@ -16,3 +16,7 @@ console.log(arrCheck([['string']]))
 console.log(arrCheck([[], {}]))
 console.log(arrCheck([[1], [2], [3]]))
 console.log(arrCheck(['A', 'R', 'R', 'A', 'Y']))
+
+// ALTERNATIVE SOLUTION
+
+// const arrCheck = a => a.every(Array.isArray) ;
