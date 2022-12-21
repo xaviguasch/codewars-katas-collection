8636c566a81988e373c2159398cd56364582b9b7
xaviguasch <xaviguasch@gmail.com>
2022-12-21 19:53:24 +0100

    added arrays 7kyu 139-numbers-to-letters kata

---
+++ 02-fundamentals--7-kyu/139-numbers-to-letters.js
@@ -0,0 +1,13 @@
+// Given an array of numbers (in string format), you must return a string. The numbers correspond to the letters of the alphabet in reverse order: a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by '27', '28' and '29' respectively.
+
+// All inputs will be valid.
+
+const switcher = (x) => {
+  return
+}
+
+console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8']))
+console.log(
+  switcher(['25', '7', '8', '4', '14', '23', '8', '25', '23', '29', '16', '16', '4'])
+)
+console.log(switcher(['4', '24']))
