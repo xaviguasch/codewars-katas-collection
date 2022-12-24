691a4856544340dc56647228ffadc93ea5ff3a05
xaviguasch <xaviguasch@gmail.com>
2022-12-24 23:22:29 +0100

    added arrays 7kyu 142-possibilites-array kata

---
+++ 02-fundamentals--7-kyu/142-possibilities-array.js
@@ -0,0 +1,15 @@
+// DESCRIPTION:
+// A non-empty array a of length n is called an array of all possibilities if it contains all numbers between [0,a.length-1].Write a method named isAllPossibilities that accepts an integer array and returns true if the array is an array of all possibilities, else false.
+
+// Example:
+
+// a=[1,2,0,3]
+// a.length-1=3
+// a includes [0,3] ,hence the function should return true
+
+const isAllPossibilities = (x) => {
+  return
+}
+
+console.log(isAllPossibilities([0, 1, 2, 3])) // true
+console.log(isAllPossibilities([1, 2, 3, 4])) // false
