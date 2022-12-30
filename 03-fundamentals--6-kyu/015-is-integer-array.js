e4938285c85adff0c6d78f1adf895b3f9d5fef4b
xaviguasch <xaviguasch@gmail.com>
2022-12-30 21:11:14 +0100

    added arrays 6kyu 015-is-integer-array kata

---
+++ 03-fundamentals--6-kyu/015-is-integer-array.js
@@ -0,0 +1,13 @@
+// Write a function with the signature shown below:
+
+// returns true  / True if every element in an array is an integer or a float with no decimals.
+// returns true  / True if array is empty.
+// returns false / False for every other input.
+
+function isIntArray(arr) {
+  return true
+}
+
+console.log(isIntArray([]))
+console.log(isIntArray([1, 2, 3, 4]))
+console.log(isIntArray([1, 2, 3, NaN]))
