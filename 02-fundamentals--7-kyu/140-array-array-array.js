3745d0b0aa43d169b59830f8468d3f26593847e8
xaviguasch <xaviguasch@gmail.com>
2023-01-06 11:51:28 +0100

    added solution to array 7kyu 140-array-array-array kata

--- 02-fundamentals--7-kyu/140-array-array-array.js
+++ 02-fundamentals--7-kyu/140-array-array-array.js
@@ -9,11 +9,33 @@
 // if (x) == ['a', 3] you should return [['a', 3], ['a', 3], ['a', 3]].
 
 const explode = (x) => {
-  return
+  let score = 0
+  let notNumbers = 0
+
+  const newArr = x.map((el) => {
+    if (isNaN(el)) {
+      notNumbers++
+    } else {
+      score += el
+    }
+    return el
+  })
+
+  if (notNumbers === 2) {
+    return 'Void!'
+  }
+
+  const resultArr = []
+
+  for (let i = 0; i < score; i++) {
+    resultArr.push(newArr)
+  }
+
+  return resultArr
 }
 
-console.log(explode([9, 3]))
-console.log(explode(['a', 3]))
-console.log(explode([6, 'c']))
-console.log(explode(['a', 'b']))
-console.log(explode(['a', 0]))
+// console.log(explode([9, 3]))
+// console.log(explode(['a', 3]))
+// console.log(explode([6, 'c']))
+// console.log(explode(['a', 'b']))
+// console.log(explode(['a', 0]))
