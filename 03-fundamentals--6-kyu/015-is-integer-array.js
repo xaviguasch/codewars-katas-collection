47af05e870f506a7cd5761b77615e5eb8532ee0e
xaviguasch <xaviguasch@gmail.com>
2023-01-13 16:27:48 +0100

    added solution to 6kyu arrays 015-is-integer-array kata

--- 03-fundamentals--6-kyu/015-is-integer-array.js
+++ 03-fundamentals--6-kyu/015-is-integer-array.js
@@ -4,7 +4,30 @@
 // returns true  / True if array is empty.
 // returns false / False for every other input.
 
-function isIntArray(arr) {}
+function isIntArray(arr) {
+  var hasNaN = false
+
+  function isInt(num) {
+    return Math.round(num) === num
+  }
+
+  if (arr instanceof Array === false) {
+    return false
+  } else if (arr.length === 0) {
+    return true
+  }
+  for (var i = 0; i < arr.length; i++) {
+    if (isInt(arr[i]) === false) {
+      hasNaN = true
+      break
+    }
+  }
+  if (hasNaN === true) {
+    return false
+  } else {
+    return true
+  }
+}
 
 console.log(isIntArray([]))
 console.log(isIntArray([1, 2, 3, 4]))
