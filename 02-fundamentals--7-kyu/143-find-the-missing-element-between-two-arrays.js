eaf6713f38d9f169fd17ac367f1bbb1dec7b1efc
xaviguasch <xaviguasch@gmail.com>
2023-01-17 13:37:54 +0100

    added alternative solutions to previous solved kata

--- 02-fundamentals--7-kyu/143-find-the-missing-element-between-two-arrays.js
+++ 02-fundamentals--7-kyu/143-find-the-missing-element-between-two-arrays.js
@@ -23,3 +23,8 @@ console.log(findMissing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]))
 console.log(findMissing([7], []))
 console.log(findMissing([4, 3, 3, 61, 8, 8], [8, 61, 8, 3, 4]))
 console.log(findMissing([0, 0, 0, 0, 0], [0, 0, 0, 0]))
+
+// ALTERNATIVE SOLUTIONS
+
+// const sum = arr => arr.reduce((a, b) => a + b, 0);
+// const findMissing = (arr1, arr2) => sum(arr1) - sum(arr2);
