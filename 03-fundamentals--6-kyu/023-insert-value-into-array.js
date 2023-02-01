84ca143184c78dd67998a55406fb45bebe9113b1
xaviguasch <xaviguasch@gmail.com>
2023-02-01 19:54:21 +0100

    added 6kyu arrays 023-insert-value-array kata

---
+++ 03-fundamentals--6-kyu/023-insert-value-into-array.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+// Extend the Array object with the function insert(index,value). This function must change the original array and insert the value at the given index in the array.
+
+// If the index is greater than the array's size, insert the value at the end.
+
+// You must return the array to be able to chain the insert function call.
+
+// You can use Array's built-in functions to help you.
+
+// The 'insert' function must not be enumerable.
+
+// Example :
+
+// [1,2,3].insert(0,42) => [42,1,2,3]
+
+// tests pending
