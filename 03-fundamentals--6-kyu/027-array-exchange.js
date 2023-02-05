f61073f1f8e1205bb064cc95232a7ae962835514
xaviguasch <xaviguasch@gmail.com>
2023-02-05 20:54:35 +0100

    added 6kyu 027-array-exchange kata

---
+++ 03-fundamentals--6-kyu/027-array-exchange.js
@@ -0,0 +1,23 @@
+// DESCRIPTION:
+// Array Exchange and Reversing
+
+// It's time for some array exchange! The objective is simple: exchange the elements of two arrays in-place in a way that their new content is also reversed.
+
+// // before
+// const myArray = ['a', 'b', 'c'];
+// const otherArray = [1, 2, 3];
+
+// exchangeWith(myArray, otherArray);
+
+// // after
+// myArray => [3, 2, 1]
+// otherArray => ['c', 'b', 'a']
+
+const exchangeWith = (a, b) => {
+  return
+}
+
+const myArray = ['a', 'b', 'c']
+const otherArray = [1, 2, 3]
+
+// console.log(exchangeWith(myArray, otherArray));
