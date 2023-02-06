4142f12ef4d8e6b9b09bb6c9f19e66e7ddca31d4
xaviguasch <xaviguasch@gmail.com>
2023-02-06 22:10:36 +0100

    added 6kyu 028-arrays-similar kata

---
+++ 03-fundamentals--6-kyu/028-arrays-similar.js
@@ -0,0 +1,24 @@
+// DESCRIPTION:
+// Write a function that determines whether the passed in arrays are similar. Similar means they contain the same elements, and the same number of occurrences of elements.
+
+// const arr1 = [1, 2, 2, 3, 4],
+//       arr2 = [2, 1, 2, 4, 3],
+//       arr3 = [1, 2, 3, 4],
+//       arr4 = [1, 2, 3, "4"]
+
+// arraysSimilar(arr1, arr2); // Should equal true
+// arraysSimilar(arr2, arr3); // Should equal false
+// arraysSimilar(arr3, arr4); // Should equal false
+
+const arraysSimilar = (arr1, arr2) => {
+  return
+}
+
+const arr1 = [1, 2, 2, 3, 4],
+  arr2 = [2, 1, 2, 4, 3],
+  arr3 = [1, 2, 3, 4],
+  arr4 = [1, 2, 3, '4']
+
+console.log(arraysSimilar(arr1, arr2))
+console.log(arraysSimilar(arr2, arr3))
+console.log(arraysSimilar(arr3, arr4))
