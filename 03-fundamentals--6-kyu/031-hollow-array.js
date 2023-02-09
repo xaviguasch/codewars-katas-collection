74c40ee03d75849e7d600d6aafd47f0f99764008
xaviguasch <xaviguasch@gmail.com>
2023-02-09 22:22:53 +0100

    added 6kyu arrays 031-hollow-array kata

---
+++ 03-fundamentals--6-kyu/031-hollow-array.js
@@ -0,0 +1,12 @@
+// DESCRIPTION:
+// An array is said to be hollow if it contains 3 or more 0s in the middle that are preceded and followed by the same number of non-zero elements. Furthermore, all the zeroes in the array must be in the middle of the array.
+
+// Write a function named isHollow/is_hollow/IsHollow that accepts an integer array and returns true if it is a hollow array,else false.
+
+const isHollow = (x) => {
+  return
+}
+
+console.log(isHollow([-1, 0, 0, 0, 3]))
+console.log(isHollow([1, 0, 0, 0, 0]))
+console.log(isHollow([100, 0, 0, 3]))
