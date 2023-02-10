4521ec55774ee0b4af860d68ccb52b2ba6d64a53
xaviguasch <xaviguasch@gmail.com>
2023-02-10 21:15:21 +0100

    added 6kyu 032-sort-arrays-ignoring-case kata

---
+++ 03-fundamentals--6-kyu/032-sort-arrays-ignoring-case.js
@@ -0,0 +1,12 @@
+// Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+// ["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+// ["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+const sortMe = (names) => {
+  return
+}
+
+console.log(sortMe(['Hello', 'there', "I'm", 'fine']))
+console.log(sortMe(['C', 'd', 'a', 'B']))
+console.log(sortMe(['CodeWars']))
