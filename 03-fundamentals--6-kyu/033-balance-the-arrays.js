c9493db49a85303d4b66ac5e5a7ab01af239495a
xaviguasch <xaviguasch@gmail.com>
2023-02-11 20:26:27 +0100

    added 6kyu 033-balance-the-arrays kata

---
+++ 03-fundamentals--6-kyu/033-balance-the-arrays.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+// Check that the two provided arrays both contain the same number of different unique items, regardless of order. For example in the following:
+
+// [a,a,a,a,b,b] and [c,c,c,d,c,d]
+// Both arrays have four of one item and two of another, so balance should return true.
+
+// #Have fun!
+
+const balance = (arr1, arr2) => {
+  return
+}
+
+console.log(balance([a, a, a, a, b, b], [c, c, c, d, c, d]))
