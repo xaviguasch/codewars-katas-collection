2ce9620c00887ef41238081b3618bfdbcbf408f5
xaviguasch <xaviguasch@gmail.com>
2023-02-16 21:03:53 +0100

    added 7kyu 144-use-map-to-double-the-values-in-an-array kata

---
+++ 02-fundamentals--7-kyu/144-use-map-to-double-the-values-in-an-array.js
@@ -0,0 +1,20 @@
+// Make the double() function return a new array with each value twice as large as the corresponding value in the passed in array. Your solution must use the map() function of the built-in javascript Array object. If you're not familiar with map(), reading over the documentation may help.
+
+// function double(array) {
+//     // Use array.map() to return a new array with each value twice
+//     // as large as the corresponding value in the passed in array.
+// }
+// For example:
+
+// var someNumbers = [1,2,10,57];
+
+// double(someNumbers); // should return [2,4,20,114]
+
+const double = (array) => {
+  // Use array.map() to return a new array with each value twice
+  // as large as the corresponding value in the passed in array.
+  return
+}
+
+console.log(double([1, 2, 3, 4, 5]))
+console.log(double([71, -548, 12.3, 31415]))
