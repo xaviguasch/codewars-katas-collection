81d572dacc8859ceab0f511315aa89d244e8d547
xaviguasch <xaviguasch@gmail.com>
2023-02-17 20:49:00 +0100

    added 7kyu 145-array-mash kata

---
+++ 02-fundamentals--7-kyu/145-array-mash.js
@@ -0,0 +1,11 @@
+// Mash 2 arrays together so that the returning array has alternating elements of the 2 arrays . Both arrays will always be the same length.
+
+// eg. [1,2,3] + ['a','b','c'] = [1, 'a', 2, 'b', 3, 'c']
+
+const arrayMash = (array1, array2) => {
+  return
+}
+
+console.log(arrayMash([1, 2, 3], ['a', 'b', 'c']))
+console.log(arrayMash([1, 2, 3, 4, 5], ['a', 'b', 'c', 'd', 'e']))
+console.log(arrayMash([1, 1, 1, 1], [2, 2, 2, 2]))
