2c914c3e85177faf0757a5954b81ee8d9050c159
xaviguasch <xaviguasch@gmail.com>
2023-02-27 20:46:22 +0100

    added 7kyu 154-index-capitalization kata

---
+++ 02-fundamentals--7-kyu/154-index-capitalization.js
@@ -0,0 +1,18 @@
+// DESCRIPTION:
+// Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+
+// For example:
+
+// capitalize("abcdef",[1,2,5]) = "aBCdeF"
+// capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+// The input will be a lowercase string with no spaces and an array of digits.
+
+// Good luck!
+
+const capitalize = (s, arr) => {
+  return
+}
+
+console.log(capitalize('abcdef', [1, 2, 5]))
+console.log(capitalize('abcdef', [1, 2, 5, 100]))
+console.log(capitalize('codewars', [1, 3, 5, 50]))
